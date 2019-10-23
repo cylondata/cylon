cf39d6cac2390d1c109c4f2c27629369f16c2939
Chathura Widanage <chathurawidanage@gmail.com>
2019-10-22 21:47:08 -0400

    Adding receiver

---
+++ src/api/comm/Receiver.h
@@ -0,0 +1,17 @@
+//
+// Created by chathura on 10/22/19.
+//
+
+#ifndef TWISTERX_RECEIVER_H
+#define TWISTERX_RECEIVER_H
+
+#include "Buffer.h"
+
+namespace twisterx::comm {
+    class Receiver {
+    public:
+        virtual void receive(int32_t source, Buffer *buffer) = 0;
+    };
+}
+
+#endif //TWISTERX_RECEIVER_H
