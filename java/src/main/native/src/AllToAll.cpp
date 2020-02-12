dfb2f75d89cd3aab3aaaef0e0c926440dde7dd11
supunkamburugamuva <supun06@gmail.com>
2020-02-12 15:47:29 -0500

    adding shared library loading

--- java/src/main/native/src/AllToAll.cpp
+++ java/src/main/native/src/AllToAll.cpp
@@ -1,1 +1,8 @@
 #include "AllToAll.h"
+
+#include "../include/org_twister2_AllToAll.h"
+#include <iostream>
+
+JNIEXPORT void JNICALL Java_org_twister2_AllToAll_WriteMessage(JNIEnv *, jobject) {
+  std::cout << "Hello JNI";
+}
