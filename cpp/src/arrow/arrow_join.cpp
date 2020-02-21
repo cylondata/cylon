7f99e53dd9068a8c91ac60fb3bfec4ddc8488387
supunkamburugamuve <supun06@gmail.com>
2020-02-21 15:52:15 -0500

    adding join all to all

--- cpp/src/arrow/arrow_join.cpp
+++ cpp/src/arrow/arrow_join.cpp
@@ -1,2 +1,37 @@
-#include "arrow_join.h"
+#include "arrow_join.hpp"
+#include "../join/tx_join.hpp";
+
+namespace twisterx {
+  ArrowJoin::ArrowJoin(int worker_id, const std::vector<int> &source, const std::vector<int> &targets, int edgeId,
+                       twisterx::JoinCallback *callback, std::shared_ptr<arrow::Schema> schema) {
+    leftCallBack_ = std::make_shared<AllToAllCallback>(leftTables_);
+    rightCallBack_ = std::make_shared<AllToAllCallback>(rightTables_);
+    leftAllToAll_ = std::make_shared<ArrowAllToAll>(worker_id, source, targets, edgeId, leftCallBack_.get(), schema);
+    rightAllToAll_ = std::make_shared<ArrowAllToAll>(worker_id, source, targets, edgeId, rightCallBack_.get(), schema);
+  }
+
+  bool ArrowJoin::isComplete() {
+    while (true) {
+      bool left = leftAllToAll_->isComplete();
+      bool right = leftAllToAll_->isComplete();
+
+      if (left && right) {
+        // join
+
+        break;
+      }
+    }
+    return true;
+  }
+
+  AllToAllCallback::AllToAllCallback(std::vector<std::shared_ptr<arrow::Table>>& table) {
+    tables_ = table;
+  }
+
+  bool AllToAllCallback::onReceive(int source, std::shared_ptr<arrow::Table> table) {
+    tables_.push_back(table);
+  }
+}
+
+
 
--- cpp/src/join/tx_join.hpp
+++ cpp/src/join/tx_join.hpp
@@ -23,7 +23,7 @@ void join(std::shared_ptr<arrow::Table> left_tab, std::shared_ptr<arrow::Table> 
 		  JoinAlgorithm join_algorithm);
 
 template<typename JOIN_COLUMN_ARRAY, typename ARROW_KEY_TYPE, typename CPP_KEY_TYPE>
-void join(std::vector<std::shared_ptr<arrow::Table>> left_tabs, std::vector<std::shared_ptr<arrow::Table>> right_tabs,
+void join(std::vector<std::shared_ptr<arrow::Table>>& left_tabs, std::vector<std::shared_ptr<arrow::Table>>& right_tabs,
 		  int64_t left_join_column_idx,
 		  int64_t right_join_column_idx,
 		  std::shared_ptr<std::unordered_map<int64_t, arrow::ArrayBuilder>> column_builders,
