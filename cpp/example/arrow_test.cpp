d502109dd1d17ad911b98c618114b3b79311fc1f
Chathura Widanage <chathurawidanage@gmail.com>
2020-02-24 11:30:35 -0500

    adding sorting and removed templates

--- cpp/example/arrow_test.cpp
+++ cpp/example/arrow_test.cpp
@@ -8,15 +8,16 @@
 #include <arrow/compute/api.h>
 #include <chrono>
 #include <ctime>
+#include <util/arrow_utils.h>
 #include "join/tx_join.cpp"
 
-#include "arrow/arrow_all_to_all.hpp"
 #include "arrow/arrow_kernels.hpp"
 
 using arrow::DoubleBuilder;
 using arrow::Int64Builder;
+using arrow::FloatBuilder;
 
-int main(int argc, char *argv[]) {
+void join_test() {
   arrow::MemoryPool *pool = arrow::default_memory_pool();
 
   Int64Builder left_id_builder(pool);
@@ -29,41 +30,41 @@ int main(int argc, char *argv[]) {
   int range = count * 10;
 
   for (int i = 0; i < count; i++) {
-    int l = rand() % range;
-    int r = rand() % range;
+	int l = rand() % range;
+	int r = rand() % range;
 
-    //LOG(INFO) << "adding " << r << "and" << l;
-    left_id_builder.Append(l);
-    right_id_builder.Append(r);
-    cost_builder.Append(i);
+	//LOG(INFO) << "adding " << r << "and" << l;
+	left_id_builder.Append(l);
+	right_id_builder.Append(r);
+	cost_builder.Append(i);
   }
 
   LOG(INFO) << "added";
 
-  std::vector <std::shared_ptr<arrow::Field>> schema_vector = {
-      arrow::field("id", arrow::int64()), arrow::field("cost", arrow::int64())};
+  std::vector<std::shared_ptr<arrow::Field>> schema_vector = {
+	  arrow::field("id", arrow::int64()), arrow::field("cost", arrow::int64())};
   auto schema = std::make_shared<arrow::Schema>(schema_vector);
 
-  std::shared_ptr <arrow::Array> left_id_array;
+  std::shared_ptr<arrow::Array> left_id_array;
   left_id_builder.Finish(&left_id_array);
-  std::shared_ptr <arrow::Array> right_id_array;
+  std::shared_ptr<arrow::Array> right_id_array;
   right_id_builder.Finish(&right_id_array);
 
-  std::shared_ptr <arrow::Array> cost_array;
+  std::shared_ptr<arrow::Array> cost_array;
   cost_builder.Finish(&cost_array);
 
-  std::shared_ptr <arrow::Table> left_table = arrow::Table::Make(schema, {left_id_array, cost_array});
-  std::shared_ptr <arrow::Table> right_table = arrow::Table::Make(schema, {right_id_array, cost_array});
+  std::shared_ptr<arrow::Table> left_table = arrow::Table::Make(schema, {left_id_array, cost_array});
+  std::shared_ptr<arrow::Table> right_table = arrow::Table::Make(schema, {right_id_array, cost_array});
 
   LOG(INFO) << "Starting join";
   auto start = std::chrono::high_resolution_clock::now();
   twisterx::join::join<arrow::Int64Array, arrow::Int64Type, int64_t>(
-      left_table,
-      right_table,
-      0,
-      0,
-      NULLPTR,
-      NULLPTR, twisterx::join::JoinType::INNER, twisterx::join::JoinAlgorithm::SORT
+	  left_table,
+	  right_table,
+	  0,
+	  0,
+	  NULLPTR,
+	  NULLPTR, twisterx::join::JoinType::INNER, twisterx::join::JoinAlgorithm::SORT
   );
   auto end = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
@@ -72,13 +73,13 @@ int main(int argc, char *argv[]) {
   arrow::Int64Builder firstBuilder(pool);
   arrow::Int32Builder targetBuilder(pool);
   for (int i = 0; i < count; i++) {
-    int l = rand() % range;
-    firstBuilder.Append(i);
-    targetBuilder.Append(i % 10);
+	int l = rand() % range;
+	firstBuilder.Append(i);
+	targetBuilder.Append(i % 10);
   }
-  std::shared_ptr <arrow::Array> firstArray;
+  std::shared_ptr<arrow::Array> firstArray;
   firstBuilder.Finish(&firstArray);
-  std::shared_ptr <arrow::Int32Array> targetArray;
+  std::shared_ptr<arrow::Int32Array> targetArray;
   targetBuilder.Finish(&targetArray);
 
   std::shared_ptr<std::vector<int>> ivec = std::make_shared<std::vector<int>>(10);
@@ -94,12 +95,52 @@ int main(int argc, char *argv[]) {
   LOG(INFO) << "Size: " << out.size();
 
   for (auto i : out) {
-    LOG(INFO) << i.first << "   " << i.second->length();
-    auto ids =
-        std::static_pointer_cast<arrow::Int64Array>(i.second);
-    for (int k = 0; k < ids->length(); k++) {
-      LOG(INFO) << "array: "  << k << ": " << ids->Value(k);
-    }
+	LOG(INFO) << i.first << "   " << i.second->length();
+	auto ids =
+		std::static_pointer_cast<arrow::Int64Array>(i.second);
+	for (int k = 0; k < ids->length(); k++) {
+	  LOG(INFO) << "array: " << k << ": " << ids->Value(k);
+	}
   }
+}
+
+void util_test() {
+  arrow::MemoryPool *pool = arrow::default_memory_pool();
+
+  Int64Builder left_id_builder(pool);
+  FloatBuilder cost_builder(pool);
+
+  srand(std::time(NULL));
+
+  int count = 400;
+  int range = count * 10;
+
+  for (int i = 0; i < count; i++) {
+	int l = rand() % range;
+	float f = l + 0.0001f;
+	//LOG(INFO) << "adding " << r << "and" << l;
+	left_id_builder.Append(l);
+	cost_builder.Append(f);
+  }
+
+  LOG(INFO) << "added";
+
+  std::vector<std::shared_ptr<arrow::Field>> schema_vector = {
+	  arrow::field("id", arrow::int64()), arrow::field("cost", arrow::float16())};
+  auto schema = std::make_shared<arrow::Schema>(schema_vector);
+
+  std::shared_ptr<arrow::Array> left_id_array;
+  left_id_builder.Finish(&left_id_array);
+  std::shared_ptr<arrow::Array> cost_array;
+  cost_builder.Finish(&cost_array);
+
+  std::shared_ptr<arrow::Table> left_table = arrow::Table::Make(schema, {left_id_array, cost_array});
+
+  std::shared_ptr<arrow::Table> sorted_table = twisterx::util::sort_table(left_table, 0, pool);
+
+}
+
+int main(int argc, char *argv[]) {
+  util_test();
   return 0;
 }
--- cpp/src/util/arrow_utils.cpp
+++ cpp/src/util/arrow_utils.cpp
@@ -1,34 +1,71 @@
 #include <arrow/compute/api.h>
-#include <arrow/array/builder_base.h>
 #include <arrow/api.h>
 #include "arrow_utils.h"
 
 namespace twisterx::util {
 
-arrow::Status copy_int64_by_index(int64_t index,
-								  const std::shared_ptr<arrow::Array>& data_column,
-								  const std::shared_ptr<arrow::ArrayBuilder>& array_builder) {
-  auto casted_builder = std::static_pointer_cast<arrow::NumericBuilder<arrow::Int64Type>>(array_builder);
-  auto casted_data_array = std::static_pointer_cast<arrow::Int64Array>(data_column);
-  return casted_builder->Append(casted_data_array->Value(index));
-}
-
-void sort_column(const std::shared_ptr<arrow::Array> &data_column,
-				 const std::shared_ptr<arrow::Int64Array> &sorted_indices,
-				 const std::shared_ptr<arrow::ArrayBuilder> &array_builder) {
-
+arrow::Status sort_column(const std::shared_ptr<arrow::Array> &data_column,
+						  std::shared_ptr<arrow::DataType> type,
+						  const std::shared_ptr<arrow::Int64Array> &sorted_indices,
+						  std::shared_ptr<arrow::Array> sorted_data_column) {
   int64_t length = sorted_indices->length();
-  arrow::Status reserveStatus = array_builder->Reserve(length);
-  for (int64_t index = 0; index < length; ++index) {
-	int64_t current_index = sorted_indices->Value(index);
-	copy_int64_by_index(current_index, data_column, array_builder);
-  }
+
+#define NUMERIC_ARRAY_SORT(TYPE) \
+{\
+  arrow::NumericBuilder<TYPE> array_builder; \
+  arrow::Status reserveStatus = array_builder.Reserve(length); \
+  auto casted_data_array = std::static_pointer_cast<arrow::NumericArray<TYPE>>(data_column); \
+  for (int64_t index = 0; index < length; ++index) { \
+    int64_t current_index = sorted_indices->Value(index); \
+    arrow::Status status = array_builder.Append(casted_data_array->Value(current_index)); \
+    if (status != arrow::Status::OK()) { \
+      return status; \
+    } \
+  } \
+  return array_builder.Finish(&sorted_data_column);\
 }
 
-template<typename JOIN_COLUMN_ARRAY, typename ARROW_KEY_TYPE, typename CPP_KEY_TYPE>
-void sort_table(std::shared_ptr<arrow::Table> tab, int64_t sort_column_index,
-				const std::shared_ptr<std::unordered_map<int64_t, std::shared_ptr<arrow::ArrayBuilder>>>& column_builders,
-				arrow::MemoryPool *memory_pool) {
+  switch (type->id()) {
+	case arrow::Type::UINT8: NUMERIC_ARRAY_SORT(arrow::UInt8Type)
+	case arrow::Type::NA:break;
+	case arrow::Type::BOOL:break;
+	case arrow::Type::INT8: NUMERIC_ARRAY_SORT(arrow::Int8Type);
+	case arrow::Type::UINT16:break;
+	case arrow::Type::INT16:break;
+	case arrow::Type::UINT32:break;
+	case arrow::Type::INT32:break;
+	case arrow::Type::UINT64:break;
+	case arrow::Type::INT64:break;
+	case arrow::Type::HALF_FLOAT:break;
+	case arrow::Type::FLOAT: NUMERIC_ARRAY_SORT(arrow::FloatType);
+	case arrow::Type::DOUBLE:break;
+	case arrow::Type::STRING:break;
+	case arrow::Type::BINARY:break;
+	case arrow::Type::FIXED_SIZE_BINARY:break;
+	case arrow::Type::DATE32:break;
+	case arrow::Type::DATE64:break;
+	case arrow::Type::TIMESTAMP:break;
+	case arrow::Type::TIME32:break;
+	case arrow::Type::TIME64:break;
+	case arrow::Type::INTERVAL:break;
+	case arrow::Type::DECIMAL:break;
+	case arrow::Type::LIST:break;
+	case arrow::Type::STRUCT:break;
+	case arrow::Type::UNION:break;
+	case arrow::Type::DICTIONARY:break;
+	case arrow::Type::MAP:break;
+	case arrow::Type::EXTENSION:break;
+	case arrow::Type::FIXED_SIZE_LIST:break;
+	case arrow::Type::DURATION:break;
+	case arrow::Type::LARGE_STRING:break;
+	case arrow::Type::LARGE_BINARY:break;
+	case arrow::Type::LARGE_LIST:break;
+  }
+#undef NUMERIC_ARRAY_SORT
+}
+
+std::shared_ptr<arrow::Table> sort_table(std::shared_ptr<arrow::Table> tab, int64_t sort_column_index,
+										 arrow::MemoryPool *memory_pool) {
   std::shared_ptr<arrow::Table> tab_to_process;
   // combine chunks if multiple chunks are available
   if (tab->column(sort_column_index)->num_chunks() > 1) {
@@ -36,8 +73,7 @@ void sort_table(std::shared_ptr<arrow::Table> tab, int64_t sort_column_index,
   } else {
 	tab_to_process = tab;
   }
-  auto column_to_sort = std::static_pointer_cast<JOIN_COLUMN_ARRAY>(
-	  tab_to_process->column(sort_column_index)->chunk(0));
+  auto column_to_sort = tab_to_process->column(sort_column_index)->chunk(0);
 
   // sort to indices
   std::shared_ptr<arrow::Array> sorted_column_index;
@@ -46,10 +82,16 @@ void sort_table(std::shared_ptr<arrow::Table> tab, int64_t sort_column_index,
   auto index_lookup = std::static_pointer_cast<arrow::Int64Array>(sorted_column_index);
 
   // now sort everything based on sorted index
+  std::vector<std::shared_ptr<arrow::Array>> sorted_columns;
   int64_t no_of_columns = tab_to_process->num_columns();
   for (int64_t col_index = 0; col_index < no_of_columns; ++col_index) {
-	sort_column(tab_to_process->column(col_index)->chunk(0), index_lookup,
-				std::shared_ptr<arrow::ArrayBuilder>(column_builders->find(col_index)->second));
+	std::shared_ptr<arrow::Array> sorted_array;
+	sort_column(tab_to_process->column(col_index)->chunk(0),
+				tab_to_process->column(0)->type(),
+				index_lookup,
+				sorted_array);
+
   }
+  return arrow::Table::Make(tab->schema(), sorted_columns);
 }
 }
