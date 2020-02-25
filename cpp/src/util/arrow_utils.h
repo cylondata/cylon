29f12a4ac9b123f1af5d1d82a372dcc9a69f87fc
Chathura Widanage <chathurawidanage@gmail.com>
2020-02-25 10:53:49 -0500

    adding copy array by indices util

--- cpp/src/util/arrow_utils.h
+++ cpp/src/util/arrow_utils.h
@@ -4,7 +4,10 @@
 
 namespace twisterx::util {
 std::shared_ptr<arrow::Table> sort_table(std::shared_ptr<arrow::Table> tab, int64_t sort_column_index,
-				arrow::MemoryPool *memory_pool);
-std::shared_ptr<arrow::Table> sort_table(std::shared_ptr<arrow::Table> tab, int64_t sort_column_index);
+										 arrow::MemoryPool *memory_pool = arrow::default_memory_pool());
+
+std::shared_ptr<arrow::Array> copy_array_by_indices(const std::shared_ptr<std::vector<int64_t>> &indices,
+													std::shared_ptr<arrow::Array> source_array,
+													arrow::MemoryPool *memory_pool = arrow::default_memory_pool());
 }
 #endif //TWISTERX_SRC_UTIL_ARROW_UTILS_H_
