5a09b0ef9a969e856293dca381d6779b77e5eb5e
Chathura Widanage <chathurawidanage@gmail.com>
2020-02-25 11:18:50 -0500

    adding join utils to a separate file

---
+++ cpp/src/join/join_utils.h
@@ -0,0 +1,14 @@
+#ifndef TWISTERX_SRC_JOIN_JOIN_UTILS_H_
+#define TWISTERX_SRC_JOIN_JOIN_UTILS_H_
+
+#include <arrow/api.h>
+#include <map>
+
+namespace twisterx::join::util {
+std::shared_ptr<arrow::Table> build_final_table(const std::shared_ptr<std::map<int64_t,
+																			   std::vector<int64_t >>> &joined_indices,
+												const std::shared_ptr<arrow::Table> &left_tab,
+												const std::shared_ptr<arrow::Table> &right_tab,
+												arrow::MemoryPool *memory_pool);
+}
+#endif //TWISTERX_SRC_JOIN_JOIN_UTILS_H_
