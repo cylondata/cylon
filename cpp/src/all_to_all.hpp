a47f6a8a565aa4f6e5a435f24967db0bad840d0e
supunkamburugamuva <supun06@gmail.com>
2020-02-27 15:03:36 -0500

    removing unwanted files

--- cpp/src/all_to_all.hpp
+++ cpp/src/all_to_all.hpp
@@ -7,9 +7,29 @@
 #include<unordered_set>
 
 #include "channel.hpp"
-#include "callback.hpp"
 
 namespace twisterx {
+  class ReceiveCallback {
+  public:
+    /**
+     * This function is called when a data is received
+     * @param source the source
+     * @param buffer the buffer allocated by the system, we need to free this
+     * @param length the length of the buffer
+     * @return true if we accept this buffer
+     */
+    virtual bool onReceive(int source, void * buffer, int length) = 0;
+
+    /**
+     * Receive the header, this happens before we receive the actual data
+     * @param source the source
+     * @param buffer the header buffer, which can be 6 integers
+     * @param length the length of the integer array
+     * @return true if we accept the header
+     */
+    virtual bool onReceiveHeader(int source, int * buffer, int length) = 0;
+  };
+
   enum AllToAllSendStatus {
     ALL_TO_ALL_SENDING,
     ALL_TO_ALL_FINISH_SENT,
