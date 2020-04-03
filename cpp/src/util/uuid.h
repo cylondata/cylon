53aa5220aba2f2eaf8334ab8978831e9a4cedace
vibhatha <vibhatha@gmail.com>
2020-04-03 15:02:50 -0400

    adding uuid cpp and cmakelist update

--- cpp/src/util/uuid.h
+++ cpp/src/util/uuid.h
@@ -5,41 +5,16 @@
 #include <sstream>
 
 namespace twisterx {
-namespace util {
-namespace uuid {
-static std::random_device rd;
-static std::mt19937 gen(rd());
-static std::uniform_int_distribution<> dis(0, 15);
-static std::uniform_int_distribution<> dis2(8, 11);
+    namespace util {
+        namespace uuid {
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+            static std::uniform_int_distribution<> dis(0, 15);
+            static std::uniform_int_distribution<> dis2(8, 11);
+            std::string generate_uuid_v4();
 
-std::string generate_uuid_v4() {
-  std::stringstream ss;
-  int i;
-  ss << std::hex;
-  for (i = 0; i < 8; i++) {
-    ss << dis(gen);
-  }
-  ss << "-";
-  for (i = 0; i < 4; i++) {
-    ss << dis(gen);
-  }
-  ss << "-4";
-  for (i = 0; i < 3; i++) {
-    ss << dis(gen);
-  }
-  ss << "-";
-  ss << dis2(gen);
-  for (i = 0; i < 3; i++) {
-    ss << dis(gen);
-  }
-  ss << "-";
-  for (i = 0; i < 12; i++) {
-    ss << dis(gen);
-  };
-  return ss.str();
-}
-}
-}
+        }
+    }
 }
 
 #endif //TWISTERX_SRC_UTIL_UUID_H_
