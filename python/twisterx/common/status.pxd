9f95f5b5faa939f961814ee4d5ed0b942b4d28bb
vibhatha <vibhatha@gmail.com>
2020-04-08 13:13:23 -0400

    adding pxd of status

---
+++ python/twisterx/common/status.pxd
@@ -0,0 +1,14 @@
+from libcpp.string cimport string
+from libcpp cimport bool
+from twisterx.common.code cimport _Code
+
+cdef extern from "../../../cpp/src/twisterx/status.cpp" namespace "twisterx":
+    cdef cppclass _Status "twisterx::Status":
+        _Status()
+        _Status(int, string)
+        _Status(int)
+        _Status(_Code)
+        _Status(_Code, string)
+        int get_code()
+        bool is_ok()
+        string get_msg()
--- python/twisterx/common/status.pyx
+++ python/twisterx/common/status.pyx
@@ -1,18 +1,7 @@
 from libcpp.string cimport string
 from libcpp cimport bool
 from twisterx.common.code cimport _Code
-
-
-cdef extern from "../../../cpp/src/twisterx/status.cpp" namespace "twisterx":
-    cdef cppclass _Status "twisterx::Status":
-        _Status()
-        _Status(int, string)
-        _Status(int)
-        _Status(_Code)
-        _Status(_Code, string)
-        int get_code()
-        bool is_ok()
-        string get_msg()
+from twisterx.common.status cimport _Status
 
 
 cdef class Status:
