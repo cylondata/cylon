1ae96fff9e7543f0c71a909becd16606d979def1
supunkamburugamuva <supun06@gmail.com>
2020-04-10 12:30:32 -0400

    adding more capabilities

--- cpp/src/twisterx/table_api.hpp
+++ cpp/src/twisterx/table_api.hpp
@@ -7,6 +7,7 @@
 
 namespace twisterx {
 
+std::shared_ptr<arrow::Table> get_table(const std::string &id);
 void put_table(const std::string &id, const std::shared_ptr<arrow::Table> &table);
 
 twisterx::Status read_csv(const std::string &path, const std::string &id);
@@ -16,26 +17,46 @@ twisterx::Status joinTables(const std::string &table_left,
                             int right_col_idx,
                             const std::string &dest_id);
 int column_count(const std::string &id);
+
+
 int row_count(const std::string &id);
+
+/**
+ * Print a table
+ * @param table_id id of the table
+ * @param col1
+ * @param col2
+ * @param row1
+ * @param row2
+ * @return
+ */
 twisterx::Status print(const std::string &table_id, int col1, int col2, int row1, int row2);
+
+/**
+ * Merge the set of tables into a single table, each table should have the same schema
+ *
+ * @param table_ids ids of the tables
+ * @param merged_tab id of the merged table
+ * @return the status of the merge
+ */
 twisterx::Status merge(std::vector<std::string> table_ids, const std::string& merged_tab);
 
 /**
  * Sort the table with the given identifier
  * @param id table id
  * @param columnIndex the sorting column index
- * @return the sorted table
+ * @return the status of the merge
  */
 twisterx::Status sortTable(const std::string& tableId, const std::string& sortTableId, int columnIndex);
 
 /**
- * Partition the table into multiple tables
- * @param id
- * @param hash_columns
- * @param no_of_partitions
- * @param out
- * @param pool
- * @return
+ * Partition the table into multiple tables using a hash function, hash will be applied to the bytes of the data
+ * @param id the table id
+ * @param hash_columns the hash columns, at the moment we only use the first column
+ * @param no_of_partitions number of partitions to output
+ * @param out tables created after hashing
+ * @param pool the memory pool
+ * @return the status of the partition operation
  */
 twisterx::Status hashPartition(const std::string& id, const std::vector<int>& hash_columns, int no_of_partitions,
                                std::vector<std::shared_ptr<arrow::Table>> *out, arrow::MemoryPool *pool);
