8d40b2864d3dcd5d5f315af098d385b6de0bc2ee
vibhatha <vibhatha@gmail.com>
2020-05-13 15:49:58 -0400

    simple mnist test

--- python/twisterx/util/data/DataManager.py
+++ python/twisterx/util/data/DataManager.py
@@ -1,4 +1,6 @@
 import os
+from math import ceil
+from math import sqrt
 import numpy as np
 import pandas as pd
 import pyarrow as pa
@@ -7,6 +9,20 @@ from pytwisterx.utils.file import path_exists
 from pytwisterx.utils.file import files_exist
 from pyarrow import csv
 from pyarrow import Table as ArrowTable
+
+
+class Partition(object):
+
+    def __init__(self, data, index):
+        self.data = data
+        self.index = index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, index):
+        data_idx = self.index[index]
+        return self.data[data_idx]
 
 
 class DataLoader(object):
@@ -58,7 +74,7 @@ class DataLoader(object):
         return self._dataset
 
     @dataset.setter
-    def dataset(self, values:List = []):
+    def dataset(self, values: List = []):
         self._dataset = values
 
     def load(self):
@@ -85,6 +101,50 @@ class LocalDataLoader(DataLoader):
             raise NotImplementedError("The Loader Type {} is not supported!".format(self.loader_type))
 
 
-
 class DistributedDataLoader(DataLoader):
     pass
+
+
+class MiniBatcher(object):
+
+    @staticmethod
+    def generate_minibatches(data=None, minibatch_size=1):
+        """
+        TODO: Write proper method definition
+        This method executes the following
+            Look total data and create mini-batches
+            Each mini-batch has a user-defined batch size
+            For instance 15,000 data divided for batch size 32
+            There can 469 partitions.
+            But we cannot use re-shape
+            create 468 * 32 = 14976 ( 468 batches)
+            and create the 469 batch with re-using already used values
+            with the remaining values 15000-14976=24
+            So adding 8 extra values from existing data
+            do this randomly
+            take a look at the code of Pytorch how they do it.
+        :param data: input data
+        :param minibatch_size: mini-batch size expected in training
+        :return: mini-batched data set
+        """
+        data_shape = data.shape
+        num_batches = ceil(data_shape[0] / float(minibatch_size))
+        remainder = data_shape[0] % minibatch_size
+        is_remainder = False
+        init_additional_idx = np.arange(5)
+        additional_records = data[init_additional_idx, :]
+        records = None
+        if remainder > 0:
+            is_remainder = True
+            additional_idx = np.arange(minibatch_size)
+            record_idx = np.arange((num_batches - 1) * minibatch_size)
+            additional_records = data[additional_idx, :]
+            additional_records_shape = additional_records.shape
+            additional_records = np.reshape(additional_records, (1, additional_records_shape[0],
+                                                                 additional_records_shape[1]))
+            records = data[record_idx, :]
+            records = np.reshape(records, (num_batches - 1, minibatch_size, data_shape[1]))
+            records = np.concatenate((records, additional_records), axis=0)
+        else:
+            records = np.reshape(data, (num_batches, minibatch_size, data_shape[1]))
+        return records
