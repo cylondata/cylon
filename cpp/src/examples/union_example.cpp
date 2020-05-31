7543a1f3271a0a1f27e129f5d1cf1f387859a257
Chathura Widanage <chathurawidanage@gmail.com>
2020-05-31 17:38:13 -0400

    adding license headers

--- cpp/src/examples/union_example.cpp
+++ cpp/src/examples/union_example.cpp
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <glog/logging.h>
 #include <net/mpi/mpi_communicator.h>
 #include <ctx/twisterx_context.h>
@@ -9,14 +23,18 @@ int main(int argc, char *argv[]) {
   auto ctx = twisterx::TwisterXContext::InitDistributed(mpi_config);
 
   std::shared_ptr<twisterx::Table> table1, table2, unioned;
-  std::string join_file = "/tmp/csv.csv";
-  auto status1 = twisterx::Table::FromCSV(join_file, &table1);
-  auto status2 = twisterx::Table::FromCSV(join_file, &table2);
+
+  LOG(INFO) << "Reading tables";
+  auto status1 = twisterx::Table::FromCSV("/home/chathura/Code/twisterx/cpp/data/csv1.csv", &table1);
+  auto status2 = twisterx::Table::FromCSV("/home/chathura/Code/twisterx/cpp/data/csv2.csv", &table2);
+  LOG(INFO) << "Done reading tables";
 
   twisterx::Status status = table1->Union(table2, unioned);
 
-  unioned->print();
-
+  LOG(INFO) << "Done union tables " << status.get_msg();
+  //unioned->print();
+  LOG(INFO) << "Table 1 had : " << table1->rows() << " and Table 2 had : " << table2->rows() << ", Union has : "
+            << unioned->rows();
   ctx->Finalize();
   return 0;
 }
