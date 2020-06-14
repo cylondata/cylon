534223171190049a1c8185ee503b5a8fc0a27b8b
niranda perera <niranda.perera@gmail.com>
2020-06-14 16:10:17 -0400

    small changes in py

--- cpp/src/examples/utils/run_hash_join.py
+++ cpp/src/examples/utils/run_hash_join.py
@@ -4,21 +4,24 @@ from os.path import expanduser
 home = expanduser("~")
 # join_exec = f"{home}/git/twisterx/build/bin/table_api_test_hash"
 join_exec = f"../../../../build/bin/table_api_test_hash"
-print(f"twx home: {join_exec}")
+print(f"twx home: {join_exec}", flush=True)
 
 csv1 = f"/tmp/csv1.csv"
 csv2 = f"/tmp/csv2.csv"
 
 out_dir = f"/tmp/twx_join_test/"
-print(f"output dir: {out_dir}")
+print(f"output dir: {out_dir}", flush=True)
 os.system(f"rm -rf {out_dir}; mkdir -p {out_dir}")
 
 cols = 4
 key_duplication_ratio = 0.99  # on avg there will be rows/key_range_ratio num of duplicate keys
 
+repetitions = 10
+print("repetitions for each test", repetitions, flush=True)
+
 # for i in [10000000]:
 for i in [int(ii * 1000000) for ii in [0.1, 1, 10, 25, 50, 75, 100]]:
-    print(f"##### test {i} starting!")
+    print(f"##### test {i} starting!", flush=True)
 
     test_dir = f"{out_dir}/{i}"
     os.system(f"rm -rf {test_dir}; mkdir -p {test_dir}")
@@ -30,7 +33,8 @@ for i in [int(ii * 1000000) for ii in [0.1, 1, 10, 25, 50, 75, 100]]:
         f"python ./generate_csv.py -o {csv2} -r {i} -c {cols} "
         f"--krange 0 {int(i * key_duplication_ratio)}")
 
-    os.system(f"{join_exec}")
+    for r in range(repetitions):
+        os.system(f"{join_exec}")
 
     # os.system(f"mv {csv1} {test_dir}")
     # os.system(f"mv {csv2} {test_dir}")
@@ -38,4 +42,4 @@ for i in [int(ii * 1000000) for ii in [0.1, 1, 10, 25, 50, 75, 100]]:
     #     os.system(f"mv /tmp/h_out_{j}.csv {test_dir}/")
     #     os.system(f"mv /tmp/s_out_{j}.csv {test_dir}/")
 
-    print(f"##### test {i} done!")
+    print(f"##### test {i} done!", flush=True)
