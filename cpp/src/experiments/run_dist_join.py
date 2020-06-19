2860e66be90320a8f63a0b9e0d279653d5a3c437
niranda perera <niranda.perera@gmail.com>
2020-06-18 23:35:00 -0400

    fixing merge conflicts

--- cpp/src/experiments/run_dist_join.py
+++ cpp/src/experiments/run_dist_join.py
@@ -2,6 +2,15 @@ import os
 from os.path import expanduser
 
 from generate_csv import generate_file
+import argparse
+
+parser = argparse.ArgumentParser(description='generate random data')
+parser.add_argument('--dry', action='store_true', help='if this is a dry run')
+
+args = parser.parse_args()
+args = vars(args)
+
+dry = args['dry']
 
 home = expanduser("~")
 # join_exec = f"{home}/git/twisterx/build/bin/table_api_test_hash"
@@ -10,11 +19,14 @@ home = expanduser("~")
 base_dir = "~/temp"
 
 csvs = [f"{base_dir}/csv1_RANK.csv", f"{base_dir}/csv2_RANK.csv"]
-row_cases = [int(ii * 1000000) for ii in [0.1, 0.25, 0.5, 0.75, 1]]
-#row_cases = [10, 20]
-
-world_sizes = [2, 4, 8, 16, 32, 64]
-#world_sizes = [2, 4, ]
+if dry:
+    row_cases = [10]
+    world_sizes = [2, ]
+    repetitions = 1
+else:
+    row_cases = [int(ii * 1000000) for ii in [0.1, 0.25, 0.5, 0.75, 1]]
+    world_sizes = [2, 4, 8, 16, 32, 64]
+    repetitions = 3
 
 out_dir = f"{base_dir}/twx_join_test/"
 print(f"\n##### output dir: {out_dir}", flush=True)
@@ -23,7 +35,6 @@ os.system(f"rm -rf {out_dir}; mkdir -p {out_dir}")
 cols = 4
 key_duplication_ratio = 0.99  # on avg there will be rows/key_range_ratio num of duplicate keys
 
-repetitions = 3
 print("\n##### repetitions for each test", repetitions, flush=True)
 
 # for i in [10000000]:
@@ -41,10 +52,12 @@ for i in row_cases:
     for w in world_sizes:
         print(f"\n\n##### rows {i} world_size {w} starting!", flush=True)
 
-        join_exec = f"mpirun --map-by node --report-bindings -mca btl vader,tcp,openib," \
-                    f"self -mca btl_tcp_if_include enp175s0f0 --mca btl_openib_allow_ib 1 " \
-                    f"--hostfile nodes -np {w} ../../../build/bin/table_join_dist_test"
-        # join_exec = f"mpirun -np {w} ../../../build/bin/table_join_dist_test"
+        if dry:
+            join_exec = f"mpirun -np {w} ../../../build/bin/table_join_dist_test"
+        else:
+            join_exec = f"mpirun --map-by node --report-bindings -mca btl vader,tcp,openib," \
+                        f"self -mca btl_tcp_if_include enp175s0f0 --mca btl_openib_allow_ib 1 " \
+                        f"--hostfile nodes -np {w} ../../../build/bin/table_join_dist_test"
         print("\n\n##### running", join_exec, flush=True)
 
         for r in range(repetitions):
@@ -59,3 +72,5 @@ for i in row_cases:
 
         print(f"\n\n##### rows {i} world_size {w} done!\n-----------------------------------------",
               flush=True)
+
+    print(f"\n\n##### rows {i} done!\n ====================================== \n", flush=True)
