10511fa2c9e3cf7100b27df6ea5e95f7f048d1d8
niranda perera <niranda.perera@gmail.com>
2020-06-21 00:03:13 -0400

    adding union dist experiments

---
+++ cpp/src/examples/table_union_dist_test.cpp
@@ -0,0 +1,101 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <net/mpi/mpi_communicator.h>
+#include <ctx/twisterx_context.h>
+#include <table.hpp>
+#include <status.hpp>
+#include <iostream>
+#include <io/csv_read_config.h>
+#include <chrono>
+
+using namespace twisterx;
+using namespace twisterx::join::config;
+
+//template <const char* jtype>
+bool RunUnion(int rank,
+              twisterx::TwisterXContext *ctx,
+              const std::shared_ptr<Table> &table1,
+              const std::shared_ptr<Table> &table2,
+              std::shared_ptr<Table> &output,
+              const string &h_out_path) {
+  Status status;
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  status = table1->DistributedUnion(ctx, table2, output);
+  auto t2 = std::chrono::high_resolution_clock::now();
+  ctx->GetCommunicator()->Barrier(); // todo: should we take this inside the dist join?
+  auto t3 = std::chrono::high_resolution_clock::now();
+
+  if (!status.is_ok()) {
+    LOG(ERROR) << "Join failed!";
+    return false;
+  }
+//    status = output->WriteCSV(h_out_path);
+//  auto t4 = std::chrono::high_resolution_clock::now();
+
+  if (status.is_ok()) {
+    LOG(INFO) << rank << " j_t " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
+              << " w_t " << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count()
+              << " lines " << output->Rows();
+    output->Clear();
+    return true;
+  } else {
+    LOG(ERROR) << "Join write failed!";
+    output->Clear();
+    return false;
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  std::shared_ptr<Table> table1, table2, joined;
+  Status status;
+
+  auto mpi_config = new twisterx::net::MPIConfig();
+  auto ctx = twisterx::TwisterXContext::InitDistributed(mpi_config);
+
+  int rank = ctx->GetRank();
+  std::string srank = std::to_string(rank);
+//  std::string base_dir = "/scratch/dnperera";
+  std::string base_dir = "/tmp";
+  system(("mkdir -p " + base_dir).c_str());
+
+  std::string csv1 = base_dir + "/csv1_" + srank + ".csv";
+  std::string csv2 = base_dir + "/csv2_" + srank + ".csv";
+
+  system(("cp ~/temp/csv1_" + srank + ".csv " + csv1).c_str());
+  system(("cp ~/temp/csv2_" + srank + ".csv " + csv2).c_str());
+
+  LOG(INFO) << rank << " Reading tables";
+  auto read_options = twisterx::io::config::CSVReadOptions().UseThreads(false).BlockSize(1 << 30);
+  if (!(status = Table::FromCSV(ctx, csv1, table1, read_options)).is_ok()) {
+    LOG(ERROR) << "File read failed! " << csv1;
+    return 1;
+  }
+  if (!(status = Table::FromCSV(ctx, csv2, table2, read_options)).is_ok()) {
+    LOG(ERROR) << "File read failed! " << csv2;
+    return 1;
+  }
+  LOG(INFO) << rank << " Done reading tables. rows " << table1->Rows() << " " << table2->Rows();
+
+  LOG(INFO) << rank << " union start";
+  RunUnion(rank, ctx, table1, table2, joined, "/scratch/dnperera/union_" + srank + ".csv");
+  LOG(INFO) << rank << " union end ----------------------------------";
+
+  ctx->Finalize();
+  return 0;
+}
+
+
---
+++ cpp/src/experiments/run_dist_union.py
@@ -0,0 +1,93 @@
+import os
+from os.path import expanduser
+
+from generate_csv import generate_file
+from multiprocessing import Process
+
+import argparse
+
+parser = argparse.ArgumentParser(description='generate random data')
+parser.add_argument('--dry', action='store_true', help='if this is a dry run')
+
+args = parser.parse_args()
+args = vars(args)
+
+dry = args['dry']
+
+home = expanduser("~")
+
+base_dir = "~/temp"
+
+csvs = [f"{base_dir}/csv1_RANK.csv", f"{base_dir}/csv2_RANK.csv"]
+if dry:
+    row_cases = [10]
+    world_sizes = [1, 2, ]
+    repetitions = 1
+else:
+    row_cases = [int(ii * 1000000) for ii in [0.125, 0.25, 0.5, 1, 2]]
+    world_sizes = [1, 2, 4, 8, 16, 32, 64, 128, 160]
+    repetitions = 4
+
+out_dir = f"{base_dir}/twx_union_test/"
+print(f"\n##### output dir: {out_dir}", flush=True)
+os.system(f"rm -rf {out_dir}; mkdir -p {out_dir}")
+
+cols = 4
+key_duplication_ratio = 0.99  # on avg there will be rows/key_range_ratio num of duplicate keys
+
+print("\n##### repetitions for each test", repetitions, flush=True)
+
+file_gen_threads = 16
+
+
+def generate_files(_rank, _i, _krange):
+    print(f"generating files for {_i} {_rank}")
+    for _f in csvs:
+        generate_file(output=_f.replace('RANK', str(_rank)), rows=_i, cols=cols, krange=_krange)
+
+
+# for i in [10000000]:
+for i in row_cases:
+    # test_dir = f"{out_dir}/{i}"
+    # os.system(f"rm -rf {test_dir}; mkdir -p {test_dir}")
+
+    for w in world_sizes:
+        krange = (0, int(i * key_duplication_ratio * w))
+
+        # generate 2 cvs for world size
+        print(f"\n\n##### generating files of rows {i} {w}!", flush=True)
+        for rank in range(0, w, file_gen_threads):
+            procs = []
+            for r in range(rank, min(rank + file_gen_threads, w)):
+                p = Process(target=generate_files, args=(r, i, krange))
+                p.start()
+                procs.append(p)
+
+            for p in procs:
+                p.join()
+
+        print(f"\n\n##### rows {i} world_size {w} starting!", flush=True)
+
+        if dry:
+            join_exec = f"mpirun -np {w} ../../../build/bin/table_union_dist_test"
+        else:
+            hostfile = "" if w == 1 else "--hostfile nodes"
+            join_exec = f"mpirun --map-by node --report-bindings -mca btl vader,tcp,openib," \
+                        f"self -mca btl_tcp_if_include enp175s0f0 --mca btl_openib_allow_ib 1 " \
+                        f"{hostfile} -np {w} ../../../build/bin/table_union_dist_test"
+        print("\n\n##### running", join_exec, flush=True)
+
+        for r in range(repetitions):
+            print(f"\n\n{i} {w} ##### {r + 1}/{repetitions} iter start!", flush=True)
+            os.system(f"{join_exec}")
+
+        # os.system(f"mv {csv1} {test_dir}")
+        # os.system(f"mv {csv2} {test_dir}")
+        # for j in ['right', 'left', 'inner', 'outer']:
+        #     os.system(f"mv /tmp/h_out_{j}.csv {test_dir}/")
+        #     os.system(f"mv /tmp/s_out_{j}.csv {test_dir}/")
+
+        print(f"\n\n##### rows {i} world_size {w} done!\n-----------------------------------------",
+              flush=True)
+
+    print(f"\n\n##### rows {i} done!\n ====================================== \n", flush=True)
