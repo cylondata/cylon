8f3271d3b675244044f343fa9c6bbc83b16b324b
niranda <niranda.perera@gmail.com>
2020-06-26 14:46:42 -0400

    minor changes to scripts

--- cpp/src/experiments/run_dist_scaling.py
+++ cpp/src/experiments/run_dist_scaling.py
@@ -11,6 +11,7 @@ parser.add_argument('-e', required=True, dest='execs', type=str, nargs='+', help
 parser.add_argument('--dry', action='store_true', help='if this is a dry run')
 parser.add_argument('--no-spark', dest='no_spark', action='store_true', help='skip spark')
 parser.add_argument('--no-twx', dest='no_twx', action='store_true', help='skip twx')
+parser.add_argument('--no-ptwx', dest='no_ptwx', action='store_true', help='skip twx')
 
 parser.add_argument('-s', required=True, dest='scaling', type=str, help='weak or strong')
 parser.add_argument('-r', dest='rows', type=float, nargs='+', help='row cases in millions',
@@ -27,6 +28,8 @@ dry = args['dry']
 execs = args['execs']
 spark = not args['no_spark']
 twx = not args['no_twx']
+ptwx = not args['no_ptwx']
+
 row_cases = [int(ii * 1000000) for ii in args['rows']]
 repetitions = args['reps']
 world_sizes = args['world']
@@ -69,6 +72,8 @@ spark_master = "spark://v-001:7077"
 
 print("\n\n##### cleaning up hdfs dfs", flush=True)
 os.system(f"{hdfs_dfs} -rm -skipTrash {dfs_base}/csv*.csv")
+
+PYTHON_EXEC = "~/victor/git/twisterx/ENV/bin/python"
 
 TOTAL_NODES = 10
 
@@ -118,9 +123,25 @@ for i in row_cases:
                     print(f"\n\n{ex} {i} {w} ##### twx {r + 1}/{repetitions} iter start! "
                           f"SPLIT_FROM_HERE", flush=True)
                     os.system(f"{join_exec}")
+        
+        if ptwx:
+            for ex in execs:
+                if ex != "table_join_dist_test":
+                    print("not suppoted", ex, flush=True)
+                    continue
+                
+                hostfile = "" if w == 1 else "--hostfile nodes"
+                join_exec = f"mpirun --map-by node --report-bindings -mca btl vader,tcp,openib," \
+                            f"self -mca btl_tcp_if_include enp175s0f0 --mca btl_openib_allow_ib 1 " \
+                            f"{hostfile} --bind-to core --bind-to socket -np {w} " \
+                            f"{PYTHON_EXEC} ../../../python/examples/experiments/{ex}.py -s {s_dir} -b {b_dir}"
+                print("\n\n##### running", join_exec, flush=True)
 
-        print(f"\n##### rows {i} world_size {w} done!\n-----------------------------------------",
-              flush=True)
+                for r in range(repetitions):
+                    print(f"\n\n{ex} {i} {w} ##### ptwx {r + 1}/{repetitions} iter start! "
+                          f"SPLIT_FROM_HERE", flush=True)
+                    os.system(f"{join_exec}")
+
 
         if spark:
             restart_spark_cluster(w)
@@ -138,5 +159,7 @@ for i in row_cases:
             print("\n\n##### cleaning up hdfs dfs", flush=True)
             os.system(f"{hdfs_dfs} -rm -skipTrash {dfs_base}/csv*.csv")
             print("\n\n##### spark done .....", flush=True)
-
+        
+        print(f"\n##### rows {i} world_size {w} done!\n-----------------------------------------", flush=True)
+        
     print(f"\n\n##### rows {i} done!\n ====================================== \n", flush=True)
