d7057d31d5325c11727a535a35034acb3b63e4d4
niranda perera <niranda.perera@gmail.com>
2020-07-08 21:57:09 -0400

    removing default paths in union example

--- cpp/src/examples/union_example.cpp
+++ cpp/src/examples/union_example.cpp
@@ -22,13 +22,9 @@
  * This example reads two csv files and does a union on them.
  */
 int main(int argc, char *argv[]) {
-  std::string path1, path2;
-  if (argc != 3) {
-    path1 = "/tmp/csv1.csv";
-    path2 = "/tmp/csv2.csv";
-  } else {
-    path1 = argv[1];
-    path2 = argv[2];
+  if (argc < 3) {
+    LOG(ERROR) << "There should be two arguments with paths to csv files";
+    return 1;
   }
 
   auto start_time = std::chrono::steady_clock::now();
@@ -39,16 +35,16 @@ int main(int argc, char *argv[]) {
   auto read_options = cylon::io::config::CSVReadOptions().UseThreads(false).BlockSize(1 << 30);
 
   // read first table
-  auto status = cylon::Table::FromCSV(ctx, path1, first_table, read_options);
+  auto status = cylon::Table::FromCSV(ctx, argv[1], first_table, read_options);
   if (!status.is_ok()) {
-    LOG(INFO) << "Table reading failed " << path1;
+    LOG(INFO) << "Table reading failed " << argv[1];
     ctx->Finalize();
     return 1;
   }
   // read second table
-  status = cylon::Table::FromCSV(ctx, path2, second_table, read_options);
+  status = cylon::Table::FromCSV(ctx, argv[2], second_table, read_options);
   if (!status.is_ok()) {
-    LOG(INFO) << "Table reading failed " << path2;
+    LOG(INFO) << "Table reading failed " << argv[2];
     ctx->Finalize();
     return 1;
   }
