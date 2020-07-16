f3e0e69957c8a8f967e9d9460a669e8395fd83a2
supunkamburugamuva <supun06@gmail.com>
2020-07-16 14:59:19 -0400

    removing unwanted file

--- cpp/src/examples/mpi_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <mpi.h>
-#include <cstdio>
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-  int x = 0;
-  std::cout << (int64_t)(&x - 1) << std::endl;
-  std::cout << (int64_t)(&x) << std::endl;
-}
