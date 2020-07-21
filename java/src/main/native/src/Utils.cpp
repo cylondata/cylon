a9f09cd395d38379dbbf8031c779cf9ce5d0db32
Chathura Widanage <chathurawidanage@gmail.com>
2020-07-21 16:42:30 -0400

    taking util functions out

---
+++ java/src/main/native/src/Utils.cpp
@@ -0,0 +1,11 @@
+#include "Utils.hpp"
+
+std::string jstr_to_str(JNIEnv *env, jstring jstr) {
+  jboolean is_copy;
+  return std::string(env->GetStringUTFChars(jstr, &is_copy));
+}
+
+void throwException(JNIEnv *env, const std::string &clazz, const std::string &msg) {
+  jclass exClass = env->FindClass(clazz.c_str());
+  env->ThrowNew(exClass, msg.c_str());
+}
