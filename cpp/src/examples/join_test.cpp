437344dd8ded392f24df008744d88f743a51e921
niranda perera <niranda.perera@gmail.com>
2020-07-23 14:19:34 -0400

    temporarily adding tests to examples

--- cpp/src/examples/join_test.cpp
+++ cpp/src/examples/join_test.cpp
@@ -16,7 +16,6 @@
 #include <net/mpi/mpi_communicator.hpp>
 #include <ctx/cylon_context.hpp>
 #include <table.hpp>
-#include <chrono>
 
 #include "test_utils.hpp"
 
@@ -25,53 +24,21 @@ using namespace cylon;
 int main(int argc, char *argv[]) {
 
   Status status;
-  auto start_start = std::chrono::steady_clock::now();
   auto mpi_config = new cylon::net::MPIConfig();
   auto ctx = cylon::CylonContext::InitDistributed(mpi_config);
 
-  int rank = ctx->GetRank();
-  int world_sz = ctx->GetWorldSize();
+  int rank = ctx->GetRank(), world_sz = ctx->GetWorldSize(), results = 0;
+  
   std::string path1 = "/tmp/cylon/input/csv1_" + std::to_string(rank) + ".csv";
   std::string path2 = "/tmp/cylon/input/csv2_" + std::to_string(rank) + ".csv";
-  std::string out_path = "/tmp/cylon/output/join_" + std::to_string(world_sz) + "_" + std::to_string(rank) + ".csv";
-
-  std::shared_ptr<cylon::Table> table1, table2, joined_expected, joined, verification;
-
-  auto read_options = cylon::io::config::CSVReadOptions().UseThreads(false);
-
-  status = cylon::Table::FromCSV(ctx,
-                                 std::vector<std::string>{path1, path2, out_path},
-                                 std::vector<std::shared_ptr<Table> *>{&table1, &table2, &joined_expected},
-                                 read_options);
-
-  if (!status.is_ok()) {
-    LOG(INFO) << "Table reading failed " << status.get_msg();
-    ctx->Finalize();
-    return 1;
+  
+  std::string out_path = "/tmp/cylon/output/join_inner_" + std::to_string(world_sz) + "_" + std::to_string(rank) + ".csv";
+  const join::config::JoinConfig &join_config = join::config::JoinConfig::InnerJoin(0, 0);
+  if (test::TestJoinOperation(join_config, ctx, path1, path2, out_path)) {
+    LOG(ERROR) << "union failed!";
+    results++;
   }
 
-  auto read_end_time = std::chrono::steady_clock::now();
-
-  LOG(INFO) << "Read tables in "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(read_end_time - start_start).count() << "[ms]";
-
-  status = table1->DistributedJoin(table2,
-                                   cylon::join::config::JoinConfig::InnerJoin(0, 0), &joined);
-  if (!status.is_ok()) {
-    LOG(INFO) << "Table join failed ";
-    ctx->Finalize();
-    return 1;
-  }
-  auto join_end_time = std::chrono::steady_clock::now();
-
-  LOG(INFO) << "First table had : " << table1->Rows() << " and Second table had : "
-            << table2->Rows() << ", Joined has : " << joined->Rows();
-  LOG(INFO) << "Join done in "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(join_end_time - read_end_time).count() << "[ms]";
-
-//  joined->WriteCSV(out_path);
-//  ctx->Finalize();
-//  return 0;
-
-  return test::Verify(ctx, joined, joined_expected);
+  ctx->Finalize();
+  return results;
 }
--- cpp/src/examples/test_utils.hpp
+++ cpp/src/examples/test_utils.hpp
@@ -11,29 +11,148 @@
 #include <table.hpp>
 #include <chrono>
 
+#define EXECUTE 1
+
 namespace cylon {
 namespace test {
-static int Verify(CylonContext *ctx, std::shared_ptr<Table> &result, std::shared_ptr<Table> &expected_result) {
+static int Verify(CylonContext *ctx, const std::shared_ptr<Table> &result,
+                  const std::shared_ptr<Table> &expected_result) {
   Status status;
   std::shared_ptr<Table> verification;
 
   LOG(INFO) << "starting verification...";
+
   LOG(INFO) << "expected:" << expected_result->Rows() << " found:" << result->Rows();
-  status = result->Subtract(expected_result, verification);
 
-  int rank = ctx->GetRank();
-
-  if (!status.is_ok() || verification->Rows()) {
-    LOG(ERROR) << "verification FAIL! Rank:" << rank << " status:" << status.get_msg() << " expected:"
-               << expected_result->Rows() << " found:" << result->Rows();
-    ctx->Finalize();
+  if (!(status = result->Subtract(expected_result, verification)).is_ok()) {
+    LOG(ERROR) << "subtract FAIL! " << status.get_msg();
+    return 1;
+  } else if (verification->Rows()) {
+    LOG(ERROR) << "verification FAIL! Rank:" << ctx->GetRank() << " status:" << status.get_msg() << " expected:"
+               << expected_result->Rows() << " found:" << result->Rows() << " " << verification->Rows();
     return 1;
   } else {
     LOG(INFO) << "verification SUCCESS!";
-    ctx->Finalize();
     return 0;
   }
 }
+
+typedef Status(*SetOperation)
+    (Table &left_table, Table &right_table, Table &out_table);
+
+static int TestSetOperation(Status(Table::*fun_ptr)(const std::shared_ptr<Table> &right, std::shared_ptr<Table> &out),
+                            CylonContext *ctx,
+                            const std::string &path1,
+                            const std::string &path2,
+                            const std::string &out_path) {
+  std::shared_ptr<cylon::Table> table1, table2, result_expected, result;
+
+  auto read_options = cylon::io::config::CSVReadOptions().UseThreads(false)
+      .WithColumnTypes({
+                           {"0", std::make_shared<DataType>(Type::INT64)},
+                           {"1", std::make_shared<DataType>(Type::DOUBLE)},
+                       });
+
+  Status status;
+  auto start_start = std::chrono::steady_clock::now();
+
+  status = cylon::Table::FromCSV(ctx,
+#if EXECUTE
+                                 std::vector<std::string>{path1, path2, out_path},
+                                 std::vector<std::shared_ptr<Table> *>{&table1, &table2, &result_expected},
+#else
+      std::vector<std::string>{path1, path2},
+      std::vector<std::shared_ptr<Table> *>{&table1, &table2},
+#endif
+                                 read_options);
+
+  if (!status.is_ok()) {
+    LOG(INFO) << "Table reading failed " << status.get_msg();
+    ctx->Finalize();
+    return 1;
+  }
+
+  auto read_end_time = std::chrono::steady_clock::now();
+
+  LOG(INFO) << "Read tables in "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(read_end_time - start_start).count() << "[ms]";
+
+  if (!(status = ((*table1).*fun_ptr)(table2, result)).is_ok()) {
+    LOG(INFO) << "Table op failed ";
+    ctx->Finalize();
+    return 1;
+  }
+  auto op_end_time = std::chrono::steady_clock::now();
+
+  LOG(INFO) << "First table had : " << table1->Rows() << " and Second table had : "
+            << table2->Rows() << ", result has : " << result->Rows();
+  LOG(INFO) << "operation done in "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(op_end_time - read_end_time).count() << "[ms]";
+
+#if EXECUTE
+  return test::Verify(ctx, result, result_expected);
+#else
+  auto write_options = io::config::CSVWriteOptions().ColumnNames(result->ColumnNames());
+  result->WriteCSV(out_path, write_options);
+  return 0;
+#endif
+}
+
+static int TestJoinOperation(const cylon::join::config::JoinConfig &join_config,
+                             CylonContext *ctx,
+                             const std::string &path1,
+                             const std::string &path2,
+                             const std::string &out_path) {
+  Status status;
+  std::shared_ptr<cylon::Table> table1, table2, joined_expected, joined, verification;
+
+  auto start_start = std::chrono::steady_clock::now();
+
+  auto read_options = cylon::io::config::CSVReadOptions().UseThreads(false);
+  status = cylon::Table::FromCSV(ctx,
+#if EXECUTE
+                                 std::vector<std::string>{path1, path2, out_path},
+                                 std::vector<std::shared_ptr<Table> *>{&table1, &table2, &joined_expected},
+#else
+      std::vector<std::string>{path1, path2},
+      std::vector<std::shared_ptr<Table> *>{&table1, &table2},
+#endif
+                                 read_options);
+
+  if (!status.is_ok()) {
+    LOG(INFO) << "Table reading failed " << status.get_msg();
+    return 1;
+  }
+
+  auto read_end_time = std::chrono::steady_clock::now();
+
+  LOG(INFO) << "Read tables in "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(read_end_time - start_start).count() << "[ms]";
+
+  status = table1->DistributedJoin(table2, join_config, &joined);
+  if (!status.is_ok()) {
+    LOG(INFO) << "Table join failed ";
+    return 1;
+  }
+  auto join_end_time = std::chrono::steady_clock::now();
+
+  LOG(INFO) << "First table had : " << table1->Rows() << " and Second table had : "
+            << table2->Rows() << ", Joined has : " << joined->Rows();
+  LOG(INFO) << "Join done in "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(join_end_time - read_end_time).count() << "[ms]";
+
+#if EXECUTE
+  if (test::Verify(ctx, joined, joined_expected)) {
+    LOG(ERROR) << "join failed!";
+    return 1;
+  }
+#else
+  auto write_options = io::config::CSVWriteOptions().ColumnNames(joined->ColumnNames());
+  joined->WriteCSV(out_path, write_options);
+#endif
+  return 0;
+}
+
 }
 }
 
