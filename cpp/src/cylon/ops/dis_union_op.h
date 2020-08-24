3a55d0a3d0a88a75e2022c6882a0679a41599df6
Chathura Widanage <chathurawidanage@gmail.com>
2020-08-23 20:18:25 -0400

    adding dis union operation

---
+++ cpp/src/cylon/ops/dis_union_op.h
@@ -0,0 +1,29 @@
+#ifndef CYLON_SRC_CYLON_OPS_DIS_UNION_OP_H_
+#define CYLON_SRC_CYLON_OPS_DIS_UNION_OP_H_
+
+#include "parallel_op.hpp"
+
+namespace cylon {
+
+class DisUnionOpConfig {
+
+};
+
+class DisUnionOp : public Op {
+
+ private:
+  std::shared_ptr<DisUnionOpConfig> config;
+
+ public:
+  DisUnionOp(std::shared_ptr<cylon::CylonContext> ctx, std::shared_ptr<arrow::Schema> schema,
+             int id, std::function<int(int)> router,
+             std::shared_ptr<ResultsCallback> callback,
+             std::shared_ptr<DisUnionOpConfig>
+             config);
+  bool Execute(int tag, std::shared_ptr<Table> table) override;
+
+  void OnParentsFinalized() override;
+  bool Finalize() override;
+};
+}
+#endif //CYLON_SRC_CYLON_OPS_DIS_UNION_OP_H_
