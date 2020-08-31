c9ef3eb388f8e1bc710cdd92b65dcb1568af36ba
Chathura Widanage <chathurawidanage@gmail.com>
2020-08-30 23:57:59 -0400

    adding a dummy join

--- cpp/src/cylon/ops/join_op.hpp
+++ cpp/src/cylon/ops/join_op.hpp
@@ -22,32 +22,28 @@ namespace cylon {
 
 class JoinOpConfig {
  private:
-  std::shared_ptr<PartitionOpConfig> partition_config;
+  int32_t join_column;
 
  public:
-  JoinOpConfig(std::shared_ptr<PartitionOpConfig> partition_config);
-
-  std::shared_ptr<PartitionOpConfig> GetPartitionConfig();
+  JoinOpConfig(int32_t join_column);
+  int32_t GetJoinColumn() const;
 };
 
 class JoinOp : public Op {
  private:
-  static const int JOIN_OP = 0;
-  static const int LEFT = 2;
-  static const int RIGHT = 3;
-
-  static const int SHUFFLE = 4;
-  static const int SORT = 5;
-  static const int LOCAL_JOIN = 6;
+  std::shared_ptr<JoinOpConfig> config;
 
  public:
   JoinOp(std::shared_ptr<CylonContext> ctx,
          std::shared_ptr<arrow::Schema> schema,
+         int32_t  id,
          std::shared_ptr<ResultsCallback> callback, std::shared_ptr<JoinOpConfig> config);
 
   bool Execute(int tag, std::shared_ptr<Table> table) override;
 
   void OnParentsFinalized() override;
+
+  bool Finalize() override;
 };
 }
 #endif //CYLON_SRC_CYLON_OPS_JOIN_OP_HPP_
