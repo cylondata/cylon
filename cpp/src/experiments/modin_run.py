277c517d8f6d94c6ad7771270137138941ae307c
niranda <niranda.perera@gmail.com>
2020-09-08 14:56:39 -0400

    working modin

--- cpp/src/experiments/modin_run.py
+++ cpp/src/experiments/modin_run.py
@@ -5,6 +5,9 @@ import time
 import argparse
 import math
 import subprocess
+import os 
+import gc
+
 
 parser = argparse.ArgumentParser(description='generate random data')
 
@@ -42,7 +45,9 @@ def start_ray(procs, nodes):
     query = ["ssh", "v-001", RAY_EXEC, "start",
              "--head", "--redis-port=6379", "--node-ip-address=v-001",
              f"--redis-password={RAY_PW}", f"--num-cpus={procs}",
-             f"--memory={20 * procs * (10 ** 9)}"]
+#              f"--memory={20 * procs * (10 ** 9)}"]
+#              "--resources={\"memory\":" + str(20 * procs * 10 ** 9) +"}"
+            ]
     print("running: ", " ".join(query), flush=True)
     subprocess.Popen(query, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
@@ -51,66 +56,94 @@ def start_ray(procs, nodes):
     for ip in ips[1:nodes]:
         print("starting worker", ip, flush=True)
         query = ["ssh", ip, RAY_EXEC, "start",
-                 "--address=\'v-001:6379\'", f"--node-ip-address={ip}",
+                 "--redis-address=\'v-001:6379\'", f"--node-ip-address={ip}",
                  f"--redis-password={RAY_PW}", f"--num-cpus={procs}",
-                 f"--memory={20 * procs * 10 ** 9}"]
+#                  f"--memory={20 * procs * 10 ** 9}"]
+#                  "--resources={\"memory\":" + str(20 * procs * 10 ** 9) +"}"
+                ]
         print("running: ", " ".join(query), flush=True)
         subprocess.Popen(query, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     time.sleep(5)
 
     import ray
-    ray.init(address='v-001:6379', redis_password=RAY_PW)
+    ray.init(redis_address='v-001:6379', redis_password=RAY_PW)
 
 
 def stop_ray():
+    import ray
+    ray.shutdown()
+
+    print("stopping workers", flush=True)
     for ip in ips:
-        print("stopping worker", ip, flush=True)
         subprocess.run(["ssh",ip , "pkill", "-f", "ray"], stdout=subprocess.PIPE,
                        stderr=subprocess.STDOUT)
-
     time.sleep(5)
-    global INIT
-    INIT = False
+#     global INIT
+#     INIT = False
 
 
 def initialize():
     global INIT
     if not INIT:
         import ray
-        ray.init(address='v-001:6379', redis_password=RAY_PW)
+        ray.init(redis_address='v-001:6379', redis_password=RAY_PW)
         INIT = True
 
 
+#awk '(NR == 1) || (FNR > 1)' csv2_*.csv > csv2_merged.csv
+def merge_files(prefix):
+    merged_f = prefix + "merged.csv"
+    if not os.path.exists(merged_f):
+        print(merged_f +" does not exist!", flush=True)        
+        s = "awk '(NR == 1) || (FNR > 1)' " + prefix + "*.csv > " + merged_f
+        print(s, flush=True)
+        os.system(s)
+    
+    return merged_f
+
+
 if __name__ == "__main__":
+    os.environ["MODIN_ENGINE"] = "ray"  # Modin will use Ray
+
     for r in rows:
         for w in world:
             procs = int(math.ceil(w / TOTAL_NODES))
-            print("procs per worker", procs, flush=True)
+            print("procs per worker", procs, " iter ", it, flush=True)
 
             assert procs <= 16
 
-            stop_ray()
-            start_ray(procs, min(w, TOTAL_NODES))
-
-            # initialize()
-
-            import modin.pandas as pd
-
-            df_l = pd.read_csv(f"~/temp/twx/{scale}/{r}/{w}/csv1_*.csv").repartition(npartitions=w)
-            df_r = pd.read_csv(f"~/temp/twx/{scale}/{r}/{w}/csv2_*.csv").repartition(npartitions=w)
-            #
-            # client.persist([df_l, df_r])
-            #
-            # print("left rows", len(df_l), flush=True)
-            # print("right rows", len(df_r), flush=True)
-
             try:
+                stop_ray()
+                start_ray(procs, min(w, TOTAL_NODES))
+                
+#                 initialize()
+
+                import modin.pandas as pd
+                pd.DEFAULT_NPARTITIONS = w
+
+                if w > 1:
+                    f_l = merge_files(f"/N/u2/d/dnperera/temp/twx/{scale}/{r}/{w}/csv1_")
+                    f_r = merge_files(f"/N/u2/d/dnperera/temp/twx/{scale}/{r}/{w}/csv2_")
+                else:
+                    f_l = f"/N/u2/d/dnperera/temp/twx/{scale}/{r}/{w}/csv1_0.csv"
+                    f_r = f"/N/u2/d/dnperera/temp/twx/{scale}/{r}/{w}/csv2_0.csv"
+                
                 for i in range(it):
+
+                    df_l = pd.read_csv(f_l)
+                    df_r = pd.read_csv(f_r)
+
+
                     t1 = time.time()
                     out = df_l.merge(df_r, on='0', how='inner', suffixes=('_left', '_right'))
                     t2 = time.time()
 
-                    print(f"###time {r} {w} {i} {(t2 - t1) * 1000:.0f}, {len(out)}", flush=True)
+                    print(f"###time {r} {w} {i} {(t2 - t1) * 1000:.0f}, {out.shape[0]}", flush=True)
+                    
+                    del df_l 
+                    del df_r
+                    del out 
+                    gc.collect()
             finally:
                 stop_ray()
