658d5e9eb14af78ea70e2893497e24746054d98e
niranda perera <niranda.perera@gmail.com>
2020-09-08 21:13:42 -0400

    pyspark

--- cpp/src/experiments/pyspark_run.py
+++ cpp/src/experiments/pyspark_run.py
@@ -1,20 +1,12 @@
-import os
-
-
-import time
-import argparse
 import math
-import subprocess
-import os 
-import gc
 import sys
-
 import time
+import gc
+
+from pyspark import SQLContext
 from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext, SQLContext
 
 TOTAL_NODES = 10
-
 
 if __name__ == "__main__":
     w = int(sys.argv[1])
@@ -29,36 +21,38 @@ if __name__ == "__main__":
     assert procs <= 16
 
     spark = SparkSession.builder \
-                        .appName(f"Spark join {w} {r}") \
-                        .config("spark.master", spark_master) \
-                        .config("spark.executor.memory", "10g") \
-                        .config("spark.executor.cores", 1) \
-                        .config("spark.memory.fraction", 0.75) \
-                        .getOrCreate()
+        .appName(f"Spark join {w}") \
+        .config("spark.master", spark_master) \
+        .config("spark.executor.memory", "10g") \
+        .config("spark.executor.cores", 1) \
+        .config("spark.memory.fraction", 0.75) \
+        .getOrCreate()
 
     spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
     sqlContext = SQLContext(sparkContext=spark.sparkContext, sparkSession=spark)
 
-    df_l = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true') \
-                .load(inputDir + "/csv1_*").repartition(w).cache()
+    df_l = sqlContext.read.format('com.databricks.spark.csv') \
+        .options(header='true', inferschema='true') \
+        .load(input_dir + "/csv1_*").repartition(w).cache()
     print("#### spark left " + df_l.count())
-    
-    df_r = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true') \
-                .load(inputDir + "/csv2_*").repartition(w).cache()
+
+    df_r = sqlContext.read.format('com.databricks.spark.csv') \
+        .options(header='true', inferschema='true') \
+        .load(input_dir + "/csv2_*").repartition(w).cache()
     print("#### spark right " + df_r.count())
 
     for i in range(it):
         t1 = time.time()
         out = df_l.join(df_r, on=['0'], how='inner')
         t2 = time.time()
-        
-        lines = q.count()
+
+        lines = out.count()
 
         print(f"##### pyark {i + 1}/{it} iter start! SPLIT_FROM_HERE", flush=True)
         print(f"###time {w} {i} {(t2 - t1) * 1000:.0f}, {lines}", flush=True)
 
 #         del df_l 
 #         del df_r
-#         del out 
-#         gc.collect()
+        del out
+        gc.collect()
 
