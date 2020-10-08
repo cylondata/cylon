42da4a162211a4ee6c23f3c15fabdb91250a26a9
vibhatha <vibhatha@gmail.com>
2020-10-08 12:06:45 -0400

    modifying ra example by col names

--- python/test/test_ra_by_column_names.py
+++ python/test/test_ra_by_column_names.py
@@ -3,31 +3,27 @@ from pycylon import Table
 from pycylon import CylonContext
 import argparse
 
-parser = argparse.ArgumentParser(description='PyCylon Table Conversion')
-parser.add_argument('--table1_path', type=str, help='Path to table 1 csv')
-parser.add_argument('--table2_path', type=str, help='Path to table 2 csv')
-
-args = parser.parse_args()
+table1_path = '/tmp/user_device_tm_1.csv'
+table2_path = '/tmp/user_usage_tm_1.csv'
 
 
-def single_process(args):
+def single_process():
     ctx: CylonContext = CylonContext(config=None)
 
-    tb1: Table = csv_reader.read(ctx, args.table1_path, ',')
+    tb1: Table = csv_reader.read(ctx, table1_path, ',')
 
-    tb2: Table = csv_reader.read(ctx, args.table2_path, ',')
+    tb2: Table = csv_reader.read(ctx, table2_path, ',')
 
     print(tb1.column_names)
     print(tb2.column_names)
 
-    configs = {'join_type': 'inner', 'algorithm': 'sort', 'left_col': 0,
-               'right_col': 0}
+    configs = {'join_type': 'inner', 'algorithm': 'sort'}
 
     tb3: Table = tb1.join(ctx, table=tb2,
                           join_type=configs['join_type'],
                           algorithm=configs['algorithm'],
                           left_on=[0],
-                          right_on=[0]
+                          right_on=[3]
                           )
 
     tb3.show()
@@ -35,8 +31,8 @@ def single_process(args):
     tb4: Table = tb1.join(ctx, table=tb2,
                           join_type=configs['join_type'],
                           algorithm=configs['algorithm'],
-                          left_on=['A'],
-                          right_on=['A']
+                          left_on=['use_id'],
+                          right_on=['use_id']
                           )
 
     tb4.show()
@@ -44,28 +40,28 @@ def single_process(args):
     tb4: Table = tb1.join(ctx, table=tb2,
                           join_type=configs['join_type'],
                           algorithm=configs['algorithm'],
-                          on=['A']
+                          on=['use_id']
                           )
 
     tb4.show()
 
-    tb5: Table = tb1.join(ctx, table=tb2,
-                          join_type=configs['join_type'],
-                          algorithm=configs['algorithm'],
-                          on=[0]
-                          )
-
-    tb5.show()
+    # tb5: Table = tb1.join(ctx, table=tb2,
+    #                       join_type=configs['join_type'],
+    #                       algorithm=configs['algorithm'],
+    #                       on=[0]
+    #                       )
+    #
+    # tb5.show()
 
     ctx.finalize()
 
 
-def multi_process(args):
+def multi_process():
     ctx: CylonContext = CylonContext(config='mpi')
 
-    tb1: Table = csv_reader.read(ctx, args.table1_path, ',')
+    tb1: Table = csv_reader.read(ctx, table1_path, ',')
 
-    tb2: Table = csv_reader.read(ctx, args.table2_path, ',')
+    tb2: Table = csv_reader.read(ctx, table2_path, ',')
 
     print(tb1.column_names)
     print(tb2.column_names)
@@ -74,41 +70,42 @@ def multi_process(args):
                'right_col': 0}
 
     tb3: Table = tb1.distributed_join(ctx, table=tb2,
-                          join_type=configs['join_type'],
-                          algorithm=configs['algorithm'],
-                          left_on=[0],
-                          right_on=[0]
-                          )
+                                      join_type=configs['join_type'],
+                                      algorithm=configs['algorithm'],
+                                      left_on=[0],
+                                      right_on=[3]
+                                      )
 
     tb3.show()
 
     tb4: Table = tb1.distributed_join(ctx, table=tb2,
-                          join_type=configs['join_type'],
-                          algorithm=configs['algorithm'],
-                          left_on=['A'],
-                          right_on=['A']
-                          )
+                                      join_type=configs['join_type'],
+                                      algorithm=configs['algorithm'],
+                                      left_on=['use_id'],
+                                      right_on=['use_id']
+                                      )
 
     tb4.show()
 
     tb4: Table = tb1.distributed_join(ctx, table=tb2,
-                          join_type=configs['join_type'],
-                          algorithm=configs['algorithm'],
-                          on=['A']
-                          )
+                                      join_type=configs['join_type'],
+                                      algorithm=configs['algorithm'],
+                                      on=['use_id']
+                                      )
 
     tb4.show()
 
-    tb5: Table = tb1.distributed_join(ctx, table=tb2,
-                          join_type=configs['join_type'],
-                          algorithm=configs['algorithm'],
-                          on=[0]
-                          )
-
-    tb5.show()
+    # tb5: Table = tb1.distributed_join(ctx, table=tb2,
+    #                       join_type=configs['join_type'],
+    #                       algorithm=configs['algorithm'],
+    #                       on=[0]
+    #                       )
+    #
+    # tb5.show()
 
     ctx.finalize()
 
-single_process(args)
 
-multi_process(args)
+single_process()
+
+multi_process()
