032c65a949913e9391b3cf461e39df883e460aba
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-11-13 14:44:19 -0500

    Fix the link of "Edit this page" (#231)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -78,15 +78,13 @@ module.exports = {
                     // It is recommended to set document id as docs home page (`docs/` path).
                     homePageId: 'compile',
                     sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/edit/master/website/',
+                        'https://github.com/cylondata/cylon/edit/master/docs/',
                 },
                 blog: {
                     showReadingTime: true,
-                    // Please change this to your repo.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                        'https://github.com/cylondata/cylon/edit/master/blog/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
