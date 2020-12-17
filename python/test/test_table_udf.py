2f173e644dd1ceb2dea7ffef4386c881caf48a02
vibhatha <vibhatha@gmail.com>
2020-12-17 09:35:19 -0500

    udf initial dev tests and benchmarks

--- python/test/test_table_udf.py
+++ python/test/test_table_udf.py
@@ -1,36 +1,82 @@
+import time
+import numpy as np
+import pandas as pd
+import pyarrow as pa
 from typing import List
-
-# class Table(object):
-#
-#     def __init__(self, data: List = None):
-#         self._tb_data = data
-#
-#     def execute(self, func):
-#         for datum in self._tb_data:
-#             func(datum)
-#
-#
-# def udf1(datum=None):
-#     print(datum)
-#
-#
-# tb = Table([1, 2, 3, 4, 5])
-#
-# tb.execute(udf1)
-
 from pyarrow.compute import add
 
 
-def addition(n):
-    return add(n, n)
+class CylonTable(object):
+    def __init__(self, tb):
+        self.table = tb
+
+    def applymap(self, func):
+        new_chunks = []
+        for chunk_array in self.table.itercolumns():
+            npr = chunk_array.to_numpy()
+            new_ca = list(map(func, npr))
+            new_chunks.append(pa.array(new_ca))
+        return pa.Table.from_arrays(new_chunks, self.table.column_names)
 
 
-# We double all numbers using map()
-import pyarrow as pa
+def benchmark_conversions():
+    N = 50_000_000
 
-a = pa.array([1, 2, 3, 4])
-numbers = (1, 2, 3, 4)
+    t1 = time.time()
+    r = np.random.random(size=N)
+    t2 = time.time()
 
-result = map(addition, a)
-rl = list(result)
-print(rl[0], type(rl[0]))
+    a = pa.array(r)
+
+    t3 = time.time()
+    npy = a.to_numpy()
+    t4 = time.time()
+
+    print(t2 - t1, t4 - t3)
+
+
+def benchmark_map_numeric():
+    N = 10
+    a_rand = np.random.random(size=N)
+    b_rand = np.random.random(size=N)
+
+    a = pa.array(a_rand)
+    b = pa.array(b_rand)
+
+    tb = pa.Table.from_arrays([a, b], ['c1', 'c2'])
+    pdf: pd.DataFrame = tb.to_pandas()
+
+    ct = CylonTable(tb)
+
+    map_func = lambda x: x + x
+    map_func = lambda x: len(str(x))
+
+    t1 = time.time()
+    new_ct = ct.applymap(map_func)
+    t2 = time.time()
+
+    t3 = time.time()
+    new_pdf = pdf.applymap(map_func)
+    t4 = time.time()
+
+    print(t2 - t1, t4 - t3)
+
+    print(new_ct.to_pandas())
+
+    print(new_pdf)
+
+
+def benchmark_non_numeric():
+    a = pa.array(['Rayan', 'Reynolds', 'Jack', 'Mat'])
+    b = pa.array(['Cameron', 'Selena', 'Roger', 'Murphy'])
+
+    tb: pa.Table = pa.Table.from_arrays([a, b], ['c1', 'c2'])
+    ct = CylonTable(tb)
+
+    map_func = lambda x: "Hello, " + x
+    new_ct = ct.applymap(map_func)
+
+    print(new_ct.to_pandas())
+
+
+benchmark_non_numeric()
