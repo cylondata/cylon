3fbb8f648b53f97ea460ab720273c533e8d5d431
vibhatha <vibhatha@gmail.com>
2020-12-18 11:55:55 -0500

    adding example cylon ops shuffle

--- python/examples/cylon_operators.py
+++ python/examples/cylon_operators.py
@@ -3,29 +3,31 @@ from pycylon import Table
 from pycylon import CylonContext
 from pycylon.net import MPIConfig
 
-mpi_config = MPIConfig()
 
-ctx = CylonContext(config=mpi_config, distributed=True)
-rows = 1000
-tb: Table = Table.from_pydict(ctx, {'c1': [i for i in range(rows)], 'c2': [i * 2 for i in range(
-    rows)], 'c3': [i * 3 for i in range(rows)]})
+def shuffle():
+    mpi_config = MPIConfig()
 
-tb: Table = Table.from_numpy(ctx, ['c1', 'c2', 'c3'], [np.random.random(size=rows),
-                                                       np.random.random(size=rows),
-                                                       np.random.random(size=rows)])
+    ctx = CylonContext(config=mpi_config, distributed=True)
+    rows = 5
+    tb: Table = Table.from_pydict(ctx, {'c1': [i for i in range(rows)], 'c2': [i * 2 for i in range(
+        rows)], 'c3': [i * 3 for i in range(rows)]})
 
-print(tb.shape)
+    tb: Table = Table.from_numpy(ctx, ['c1', 'c2', 'c3'], [np.random.random(size=rows),
+                                                           np.random.random(size=rows),
+                                                           np.random.random(size=rows)])
 
-tb_shuffle = tb.shuffle(['c1'])
+    print(tb.shape)
 
-tb_shuffle_dna = tb_shuffle.dropna(axis=1, how='all')
+    tb_shuffle = tb.shuffle(['c1'])
 
-print("Rank : ", ctx.get_rank(), tb_shuffle.shape, tb.shape, tb_shuffle_dna.shape)
+    tb_shuffle_dna = tb_shuffle.dropna(axis=1, how='all')
 
-from pycylon.io import CSVWriteOptions
+    print("Rank : ", ctx.get_rank(), tb_shuffle.shape, tb.shape, tb_shuffle_dna.shape)
 
-csv_write_options = CSVWriteOptions().with_delimiter(',')
-#
-#tb_shuffle.to_csv(f'/tmp/shuffle_{rows}_{ctx.get_rank()}.csv', csv_write_options)
+    from pycylon.io import CSVWriteOptions
 
-ctx.finalize()
+    csv_write_options = CSVWriteOptions().with_delimiter(',')
+    #
+    # tb_shuffle.to_csv(f'/tmp/shuffle_{rows}_{ctx.get_rank()}.csv', csv_write_options)
+
+    ctx.finalize()
