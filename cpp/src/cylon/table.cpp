768652d144ed0cdfbdd8deab624d00b81bdad0a9
niranda perera <niranda.perera@gmail.com>
2020-12-20 11:27:02 -0500

    move comparators to header file

--- cpp/src/cylon/table.cpp
+++ cpp/src/cylon/table.cpp
@@ -27,7 +27,6 @@
 #include  "util/to_string.hpp"
 #include "iostream"
 #include "util/arrow_utils.hpp"
-#include "arrow/arrow_partition_kernels.hpp"
 #include "util/uuid.hpp"
 #include "arrow/arrow_all_to_all.hpp"
 #include "arrow/arrow_comparator.hpp"
@@ -36,127 +35,6 @@
 #include "partition/partition.hpp"
 
 namespace cylon {
-
-class RowComparator {
- private:
-  const std::shared_ptr<arrow::Table> *tables;
-  std::shared_ptr<cylon::TableRowComparator> comparator;
-  std::shared_ptr<cylon::RowHashingKernel> row_hashing_kernel;
-  int64_t *eq, *hs;
-
- public:
-  RowComparator(std::shared_ptr<cylon::CylonContext> &ctx,
-                const std::shared_ptr<arrow::Table> *tables,
-                int64_t *eq,
-                int64_t *hs) {
-    this->tables = tables;
-    this->comparator = std::make_shared<cylon::TableRowComparator>(tables[0]->fields());
-    this->row_hashing_kernel = std::make_shared<cylon::RowHashingKernel>(tables[0]->fields());
-    this->eq = eq;
-    this->hs = hs;
-  }
-
-  // equality
-  bool operator()(const std::pair<int8_t, int64_t> &record1,
-                  const std::pair<int8_t, int64_t> &record2) const {
-    (*this->eq)++;
-    return this->comparator->compare(this->tables[record1.first], record1.second,
-                                     this->tables[record2.first], record2.second) == 0;
-  }
-
-  // hashing
-  size_t operator()(const std::pair<int8_t, int64_t> &record) const {
-    (*this->hs)++;
-    size_t hash = this->row_hashing_kernel->Hash(this->tables[record.first], record.second);
-    return hash;
-  }
-};
-
-class TableRowIndexComparator {
- public:
-  TableRowIndexComparator(const std::shared_ptr<arrow::Table> &table, const std::vector<int> &col_ids)
-      : idx_comparators_ptr(std::make_shared<std::vector<std::shared_ptr<ArrayIndexComparator>>>(col_ids.size())) {
-    for (int c:col_ids) {
-      const std::shared_ptr<arrow::Array> &array = table->column(c)->chunk(0);
-      idx_comparators_ptr->at(c) = CreateArrayIndexComparator(array);
-    }
-  }
-
-  // equality
-  bool operator()(const int64_t &record1, const int64_t &record2) const {
-    bool res = true;
-    for (auto &&comp:*idx_comparators_ptr) {
-      res &= (bool) comp->compare(record1, record2);
-    }
-    return res;
-  }
-
- private:
-  // this class gets copied to std container, so we don't want to copy these vectors.
-  // hence they are wrapped around smart pointers
-  std::shared_ptr<std::vector<std::shared_ptr<ArrayIndexComparator>>> idx_comparators_ptr;
-};
-
-class TableRowIndexHash {
- public:
-  TableRowIndexHash(const std::shared_ptr<arrow::Table> &table, const std::vector<int> &col_ids)
-      : hashes_ptr(std::make_shared<std::vector<uint32_t>>(table->num_rows(), 0)) {
-    for (auto &&c:col_ids) {
-      const std::unique_ptr<HashPartitionKernel> &hash_kernel = CreateHashPartitionKernel(table->field(c)->type());
-      hash_kernel->UpdateHash(table->column(c), *hashes_ptr); // update the hashes
-    }
-  }
-
-  // hashing
-  size_t operator()(const int64_t &record) const {
-    return hashes_ptr->at(record);
-  }
-
- private:
-  // this class gets copied to std container, so we don't want to copy these vectors.
-  // hence they are wrapped around smart pointers
-  std::shared_ptr<std::vector<uint32_t>> hashes_ptr;
-};
-
-class MultiTableRowIndexHash {
- public:
-  explicit MultiTableRowIndexHash(const std::vector<std::shared_ptr<arrow::Table>> &tables)
-      : hashes_ptr(std::make_shared<std::vector<TableRowIndexHash>>()) {
-    for (const auto &t:tables) {
-      std::vector<int> cols(t->num_columns());
-      std::iota(cols.begin(), cols.end(), 0);
-      hashes_ptr->emplace_back(t, cols);
-    }
-  }
-
-  void InsertTable(const std::shared_ptr<arrow::Table> &table, const std::vector<int> &col_ids) {
-    hashes_ptr->emplace_back(table, col_ids);
-  }
-
-  // hashing
-  size_t operator()(const std::pair<int8_t, int64_t> &record) const {
-    return hashes_ptr->at(record.first)(record.second);
-  }
-
- private:
-  // this class gets copied to std container, so we don't want to copy these vectors.
-  std::shared_ptr<std::vector<TableRowIndexHash>> hashes_ptr;
-};
-
-class MultiTableRowIndexComparator {
- public:
-  explicit MultiTableRowIndexComparator(const std::vector<std::shared_ptr<arrow::Table>> &tables)
-      : tables(tables), comparator(std::make_shared<TableRowComparator>(tables[0]->fields())) {}
-
-  bool operator()(const std::pair<int8_t, int64_t> &record1, const std::pair<int8_t, int64_t> &record2) const {
-    return this->comparator->compare(this->tables[record1.first], record1.second,
-                                     this->tables[record2.first], record2.second) == 0;
-  }
-
- private:
-  const std::vector<std::shared_ptr<arrow::Table>> &tables;
-  std::shared_ptr<TableRowComparator> comparator;
-};
 
 /**
  * creates an Arrow array based on col_idx, filtered by row_indices
