005cd7795982e7a4f8a54eb9247114f661cb0d6b
niranda perera <niranda.perera@gmail.com>
2020-12-22 17:13:25 -0500

    Revert "template copy array"
    This reverts commit 8cfbfdd7

--- cpp/src/cylon/table.cpp
+++ cpp/src/cylon/table.cpp
@@ -63,27 +63,6 @@ Status PrepareArray(std::shared_ptr<cylon::CylonContext> &ctx,
   return Status::OK();
 }
 
-template<typename ITER>
-static inline Status prepare_array(std::shared_ptr<cylon::CylonContext> &ctx,
-                                   const std::shared_ptr<arrow::Table> &table,
-                                   const int32_t col_idx,
-                                   const ITER &indices_begin,
-                                   const ITER &indices_end,
-                                   int64_t size,
-                                   arrow::ArrayVector &array_vector) {
-  std::shared_ptr<arrow::Array> destination_col_array;
-  arrow::Status ar_status = cylon::util::copy_array_by_indices(indices_begin, indices_end, size,
-                                                               table->column(col_idx)->chunk(0),
-                                                               destination_col_array, cylon::ToArrowPool(ctx));
-  if (!ar_status.ok()) {
-    LOG(FATAL) << "Failed while copying a column to the final table from tables."
-               << ar_status.ToString();
-    return Status(static_cast<int>(ar_status.code()), ar_status.message());
-  }
-  array_vector.push_back(destination_col_array);
-  return Status::OK();
-}
-
 static inline Status all_to_all_arrow_tables(std::shared_ptr<CylonContext> &ctx,
                                              const std::shared_ptr<arrow::Schema> &schema,
                                              const std::vector<std::shared_ptr<arrow::Table>> &partitioned_tables,
