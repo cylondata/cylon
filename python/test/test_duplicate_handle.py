5522737f1dbd2958b9250a9a9075de13e878da8b
vibhatha <vibhatha@gmail.com>
2020-12-27 12:52:55 -0500

    fixed test case for drop duplicates and added license for benchmark ops, tests

--- python/test/test_duplicate_handle.py
+++ python/test/test_duplicate_handle.py
@@ -1,3 +1,17 @@
+##
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
 import pytest
 import pandas as pd
 import pycylon as cn
@@ -5,6 +19,10 @@ from pycylon.net import MPIConfig
 from pycylon.io import CSVReadOptions
 from pycylon.io import read_csv
 
+'''
+Run test:
+>> mpirun -n 2 python -m pytest --with-mpi -q python/test/test_duplicate_handle.py
+'''
 
 @pytest.mark.mpi
 def test_shuffle():
@@ -17,8 +35,9 @@ def test_shuffle():
     tb: cn.Table = None
 
     rank = ctx.get_rank()
+    size = ctx.get_world_size()
 
-    assert rank == 2
+    assert size == 2
 
     if rank == 0:
         tb = cn.Table.from_pydict(ctx, {'c1': [1, 1, 3, 3, 4, 5], 'c2': [2, 2, 2, 4, 6, 6],
@@ -36,7 +55,7 @@ def test_shuffle():
 
     tb = tb.distributed_unique(['c1', 'c2', 'c3'])
 
-    ctx.finalize()
+    # NOTE: do not call ctx.finalize() since a pytest is doing a finalize call
 
 
 def test_unique():
@@ -57,3 +76,5 @@ def test_unique():
     assert sort_col == expected_indices_of_sort_col
 
     assert pdf2['d'].values.tolist() == sort_col
+
+    ctx.finalize()
