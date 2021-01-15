646b89b87b64da95a35cfc8d53dfc62875394937
vibhatha <vibhatha@gmail.com>
2021-01-15 08:30:15 -0500

    adding indexer abstracts for location finders

--- cpp/src/cylon/indexing/indexer.cpp
+++ cpp/src/cylon/indexing/indexer.cpp
@@ -1,3 +1,28 @@
 
 #include "indexer.hpp"
 
+cylon::BaseIndexer::BaseIndexer(std::shared_ptr<cylon::BaseIndex> index) {
+
+}
+void cylon::BaseIndexer::loc(void *start_index,
+                             void *end_index,
+                             int column_index,
+                             std::shared_ptr<cylon::Table> &input_table,
+                             std::shared_ptr<cylon::Table> &output) {
+
+}
+void cylon::BaseIndexer::loc(void *start_index,
+                             void *end_index,
+                             int start_column_index,
+                             int end_column_index,
+                             std::shared_ptr<cylon::Table> &input_table,
+                             std::shared_ptr<cylon::Table> &output) {
+
+}
+void cylon::BaseIndexer::loc(void *start_index,
+                             void *end_index,
+                             std::vector<int> &columns,
+                             std::shared_ptr<cylon::Table> &input_table,
+                             std::shared_ptr<cylon::Table> &output) {
+
+}
--- cpp/src/cylon/indexing/indexer.hpp
+++ cpp/src/cylon/indexing/indexer.hpp
@@ -2,8 +2,46 @@
 #ifndef CYLON_SRC_CYLON_INDEXING_INDEXER_H_
 #define CYLON_SRC_CYLON_INDEXING_INDEXER_H_
 
-class Indexer {
+#include "index.hpp"
+#include "table.hpp"
+
+namespace cylon {
+
+class BaseIndexer {
+
+ public:
+  BaseIndexer(std::shared_ptr<cylon::BaseIndex> index);
+
+  void loc(void *start_index,
+           void *end_index,
+           int column_index,
+           std::shared_ptr<cylon::Table> &input_table,
+           std::shared_ptr<cylon::Table> &output);
+
+  void loc(void *start_index,
+           void *end_index,
+           int start_column_index,
+           int end_column_index,
+           std::shared_ptr<cylon::Table> &input_table,
+           std::shared_ptr<cylon::Table> &output);
+
+  void loc(void *start_index,
+           void *end_index,
+           std::vector<int> &columns,
+           std::shared_ptr<cylon::Table> &input_table,
+           std::shared_ptr<cylon::Table> &output);
+
+ private:
+  std::shared_ptr<BaseIndexer> base_index_;
+
 
 };
+
+template<class ARROW_T, typename CTYPE = typename ARROW_T::c_type>
+class Indexer{
+
+};
+
+}
 
 #endif //CYLON_SRC_CYLON_INDEXING_INDEXER_H_
