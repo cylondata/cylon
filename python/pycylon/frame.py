53533b5b057d37c5f9a23cf4ad392c6cd276bda6
Chathura Widanage <chathurawidanage@gmail.com>
2021-03-29 18:08:48 -0400

    adding drop_duplicates

--- python/pycylon/frame.py
+++ python/pycylon/frame.py
@@ -1551,3 +1551,72 @@ class DataFrame(object):
             return DataFrame(current_table)
         else:
             raise "Unsupported operation"
+
+    def drop_duplicates(
+        self,
+        subset: Optional[Union[Hashable, Sequence[Hashable]]] = None,
+        keep: Union[str, bool] = "first",
+        inplace: bool = False,
+        ignore_index: bool = False,
+    ) -> DataFrame:
+        """
+        Return DataFrame with duplicate rows removed.
+        Considering certain columns is optional. Indexes, including time indexes
+        are ignored.
+        Parameters
+        ----------
+        subset : column label or sequence of labels, optional
+            Only consider certain columns for identifying duplicates, by
+            default use all of the columns.
+        keep : {'first', 'last', False}, default 'first'
+            Determines which duplicates (if any) to keep.
+            - ``first`` : Drop duplicates except for the first occurrence.
+            - ``last`` : Drop duplicates except for the last occurrence.
+            - False (Unsupported): Drop all duplicates.
+        inplace : bool, default False
+            Whether to drop duplicates in place or to return a copy.
+        ignore_index (Unsupported) : bool, default False
+            If True, the resulting axis will be labeled 0, 1, …, n - 1.
+            .. versionadded:: 1.0.0
+        Returns
+        -------
+        DataFrame or None
+            DataFrame with duplicates removed or None if ``inplace=True``(Unsupported).
+        See Also
+        --------
+        DataFrame.value_counts: Count unique combinations of columns.
+        Examples
+        --------
+        Consider dataset containing ramen rating.
+        >>> df = pd.DataFrame({
+        ...     'brand': ['Yum Yum', 'Yum Yum', 'Indomie', 'Indomie', 'Indomie'],
+        ...     'style': ['cup', 'cup', 'cup', 'pack', 'pack'],
+        ...     'rating': [4, 4, 3.5, 15, 5]
+        ... })
+        >>> df
+            brand style  rating
+        0  Yum Yum   cup     4.0
+        1  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        3  Indomie  pack    15.0
+        4  Indomie  pack     5.0
+        By default, it removes duplicate rows based on all columns.
+        >>> df.drop_duplicates()
+            brand style  rating
+        0  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        3  Indomie  pack    15.0
+        4  Indomie  pack     5.0
+        To remove duplicates on specific column(s), use ``subset``.
+        >>> df.drop_duplicates(subset=['brand'])
+            brand style  rating
+        0  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        To remove duplicates and keep last occurrences, use ``keep``.
+        >>> df.drop_duplicates(subset=['brand', 'style'], keep='last')
+            brand style  rating
+        1  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        4  Indomie  pack     5.0
+        """
+        return DataFrame(self._table.unique(columns=subset, keep=keep, inplace=inplace))
--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from pycylon import DataFrame
 
 df1 = DataFrame([[1, 2, 3], [2, 3, 4]])
 df2 = DataFrame([[1, 1, 1], [2, 3, 4]])
-df3 = DataFrame([[8, 9, 10], [21, 31, 41]])
+df3 = DataFrame([[8, 8, 10], [21, 31, 41]])
 
 
-joined = DataFrame.concat(objs=[df1,df2, df3])
+df3 = df3.drop_duplicates(subset=[0,1], keep='first')
 
-print(joined)
+print(df3)
