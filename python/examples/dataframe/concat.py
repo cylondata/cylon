ec964d89285a96e6b931d2a70fbb950b8716314a
niranda perera <niranda.perera@gmail.com>
2021-04-29 16:16:29 -0400

    Bug fixes in dataframe  (#420)
    * fixing #415 and #416
    
    * changes to concat operation
    
    * unwrapping sort options onto sort method

--- python/examples/dataframe/concat.py
+++ python/examples/dataframe/concat.py
@@ -1,17 +1,25 @@
-from pycylon import DataFrame, CylonEnv
-from pycylon.net import MPIConfig
 import random
 
-df1 = DataFrame([random.sample(range(10, 100), 50),
-                 random.sample(range(10, 100), 50)])
-df2 = DataFrame([random.sample(range(10, 100), 50),
-                 random.sample(range(10, 100), 50)])
-df3 = DataFrame([random.sample(range(10, 100), 50),
-                 random.sample(range(10, 100), 50)])
+import pycylon as cn
+from pycylon import DataFrame, CylonEnv
+from pycylon.net import MPIConfig
+
+df1 = DataFrame([random.sample(range(10, 100), 5),
+                 random.sample(range(10, 100), 5)])
+df2 = DataFrame([random.sample(range(10, 100), 5),
+                 random.sample(range(10, 100), 5)])
+df3 = DataFrame([random.sample(range(10, 100), 10),
+                 random.sample(range(10, 100), 10)])
 
 # local unique
-df4 = df1.concat(axis=0, objs=[df2, df3])
-print("Local Unique")
+df4 = cn.concat(axis=0, objs=[df1, df2, df3])
+print("Local concat axis0")
+print(df4)
+
+df2.rename(['00', '11'])
+df3.rename(['000', '111'])
+df4 = cn.concat(axis=1, objs=[df1, df2, df3])
+print("Local concat axis1")
 print(df4)
 
 # distributed unique
@@ -21,10 +29,16 @@ df1 = DataFrame([random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5),
                  random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5)])
 df2 = DataFrame([random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5),
                  random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5)])
-df3 = DataFrame([random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5),
-                 random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5)])
-print("Distributed Unique", env.rank)
-df4 = df1.concat(axis=0, objs=[df2, df3], env=env)
+df3 = DataFrame([random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 10),
+                 random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 10)])
+print("Distributed concat axis0", env.rank)
+df4 = cn.concat(axis=0, objs=[df1, df2, df3], env=env)
+print(df4)
+
+df2.rename(['00', '11'])
+df3.rename(['000', '111'])
+df4 = cn.concat(axis=1, objs=[df1, df2, df3], env=env)
+print("Distributed concat axis1", env.rank)
 print(df4)
 
 env.finalize()
--- python/examples/dataframe/sort.py
+++ python/examples/dataframe/sort.py
@@ -1,10 +1,10 @@
+import random
+
 from pycylon import DataFrame, CylonEnv
 from pycylon.net import MPIConfig
-import random
 
 df1 = DataFrame([random.sample(range(10, 100), 50),
                  random.sample(range(10, 100), 50)])
-
 
 # local sort
 df3 = df1.sort_values(by=[0])
@@ -14,10 +14,16 @@ print(df3)
 # distributed sort
 env = CylonEnv(config=MPIConfig())
 
-df1 = DataFrame([random.sample(range(10*env.rank, 15*(env.rank+1)), 5),
-                 random.sample(range(10*env.rank, 15*(env.rank+1)), 5)])
+df1 = DataFrame([random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5),
+                 random.sample(range(10 * env.rank, 15 * (env.rank + 1)), 5)])
 print("Distributed Sort", env.rank)
 df3 = df1.sort_values(by=[0], env=env)
+print(df3)
+
+# distributed sort
+print("Distributed Sort with sort options", env.rank)
+bins = env.world_size * 2
+df3 = df1.sort_values(by=[0], num_bins=bins, num_samples=bins, env=env)
 print(df3)
 
 env.finalize()
