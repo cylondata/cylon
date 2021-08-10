bf55525fe572b5b9f286233af7c7d95088698406
Chathura Widanage <7312649+chathurawidanage@users.noreply.github.com>
2021-08-10 16:43:14 -0400

    more windows changes

--- build.py
+++ build.py
@@ -19,70 +19,103 @@ parser = argparse.ArgumentParser()
 # C++ Build
 cpp_build = parser.add_argument_group("Cylon C++")
 cpp_build.add_argument("--cpp", action='store_true', help='Build C++ Core')
-cpp_build.add_argument("--test", action='store_true', help='Run C++ test suite')
-cpp_build.add_argument("--style-check", action='store_true', help='Compile C++ with style check')
-cpp_build.add_argument("-spath", help='C++ source directory', default=os.path.join(os.getcwd(),'cpp'))
-cpp_build.add_argument("-cmake-flags", help='Additional cmake flags', default='')
+cpp_build.add_argument("--test", action='store_true',
+                       help='Run C++ test suite')
+cpp_build.add_argument("--style-check", action='store_true',
+                       help='Compile C++ with style check')
+cpp_build.add_argument("-spath", help='C++ source directory',
+                       default=os.path.join(os.getcwd(), 'cpp'))
+cpp_build.add_argument(
+    "-cmake-flags", help='Additional cmake flags', default='')
 
 # Build mode
-build_mode = parser.add_argument_group("Build Mode").add_mutually_exclusive_group()
-build_mode.add_argument("--debug", action='store_true', help='Build the core in debug mode')
-build_mode.add_argument("--release", action='store_true', help='Build the core in release mode')
+build_mode = parser.add_argument_group(
+    "Build Mode").add_mutually_exclusive_group()
+build_mode.add_argument("--debug", action='store_true',
+                        help='Build the core in debug mode')
+build_mode.add_argument("--release", action='store_true',
+                        help='Build the core in release mode')
 
 # Python build
 python_build = parser.add_argument_group("PyCylon")
-python_build.add_argument("--python", action='store_true', help='Build PyCylon wrapper')
-python_build.add_argument("--pytest", action='store_true', help='Run Python test suite')
-python_build.add_argument("--cython", action='store_true', help='Build Cython modules')
-python_build.add_argument("--python_with_pyarrow", action='store_true', help='Build PyArrow with Python')
-python_build.add_argument("-pyenv", help='Path to Python virtual environment', default=os.path.join(os.getcwd(),'ENV'))
+python_build.add_argument(
+    "--python", action='store_true', help='Build PyCylon wrapper')
+python_build.add_argument(
+    "--pytest", action='store_true', help='Run Python test suite')
+python_build.add_argument(
+    "--cython", action='store_true', help='Build Cython modules')
+python_build.add_argument("--python_with_pyarrow",
+                          action='store_true', help='Build PyArrow with Python')
+python_build.add_argument("-pyenv", help='Path to Python virtual environment',
+                          default=os.path.join(os.getcwd(), 'ENV'))
 
 
 # Java build
 java_build = parser.add_argument_group("JCylon")
-java_build.add_argument("--java", action='store_true', help='Build JNI based Java wrapper')
+java_build.add_argument("--java", action='store_true',
+                        help='Build JNI based Java wrapper')
 
 # Docker build
 java_build = parser.add_argument_group("Docker")
-java_build.add_argument("--docker", action='store_true', help='Build Cylon Docker images locally')
+java_build.add_argument("--docker", action='store_true',
+                        help='Build Cylon Docker images locally')
 
 # Paths
-parser.add_argument("-bpath", help='Build directory', default=os.path.join(os.getcwd(),'build'))
+parser.add_argument("-bpath", help='Build directory',
+                    default=os.path.join(os.getcwd(), 'build'))
+parser.add_argument("-ipath", help='Install directory')
 
 args = parser.parse_args()
 
 # Variables
+
+
 def on_off(arg):
     return "ON" if arg else "OFF"
 
-CPP_BUILD_MODE =  "Release" if args.release else "Debug"
-CPP_SOURCE_DIR = args.spath
-RUN_CPP_TESTS = on_off(args.test)
+
+BUILD_CPP = args.cpp
+CPP_BUILD_MODE = "Release" if (args.release or (
+    not args.release and not args.debug)) else "Debug"
+CPP_SOURCE_DIR = os.path.join(args.spath)
+RUN_CPP_TESTS = args.test
 CMAKE_FLAGS = args.cmake_flags
+CPPLINT_COMMAND = " \"-DCMAKE_CXX_CPPLINT=cpplint;--linelength=100;--headers=h,hpp;--filter=-legal/copyright,-build/c++11,-runtime/references\" " if args.style_check else ""
 
-PYTHON_ENV_PATH = args.pyenv.replace('\\','/') # arrow build expects /s even on windows
-BUILD_PYTHON =on_off(args.python)
+# arrow build expects /s even on windows
+PYTHON_ENV_PATH = os.path.join(args.pyenv)
+BUILD_PYTHON = args.python
 
-BUILD_DIR = args.bpath
+# docker
+BUILD_DOCKER = args.docker
+
+BUILD_DIR = os.path.join(args.bpath)
+INSTALL_COMMAND = f"-DCMAKE_INSTALL_PREFIX={args.ipath}" if args.ipath else ""
 
 # create build directory
 if not os.path.exists(BUILD_DIR):
+    print("Creating build directory...")
     os.makedirs(BUILD_DIR)
+
 
 def env_activate_command():
     if os.name == 'posix':
-        return os.path.join(PYTHON_ENV_PATH,'bin/activate')
+        return os.path.join(PYTHON_ENV_PATH, 'bin', 'activate')
     elif os.name == 'nt':
-        return os.path.join(PYTHON_ENV_PATH,'Scripts\\activate.bat')
+        return os.path.join(PYTHON_ENV_PATH, 'Scripts', 'activate.bat')
+
 
 def read_python_requirements():
     return subprocess.call(env_activate_command()+' && pip3 install -r requirements.txt || exit 1', shell=True)
 
+
 if BUILD_PYTHON:
     read_python_requirements()
 
+
 def print_line():
-     print("=================================================================")
+    print("=================================================================")
+
 
 def check_status(status, task):
     if status != 0:
@@ -91,19 +124,24 @@ def check_status(status, task):
     else:
         print(f'{task} completed successfully')
 
+
 def build_cpp():
+    if not BUILD_CPP:
+        return
+
     print_line()
-    print("Building CPP in", CPP_BUILD_MODE,"mode")
+    print("Building CPP in", CPP_BUILD_MODE, "mode")
     print_line()
 
-    cmake_command = f'cd {BUILD_DIR} && cmake -DPYCYLON_BUILD={BUILD_PYTHON} -DPYTHON_EXEC_PATH=${PYTHON_ENV_PATH} \
-      -DCMAKE_BUILD_TYPE=${CPP_BUILD_MODE} -DCYLON_WITH_TEST=${RUN_CPP_TESTS} $CPPLINT_CMD $INSTALL_CMD \
+    cmake_command = f'cd {BUILD_DIR} && cmake -DPYCYLON_BUILD={on_off(BUILD_PYTHON)} -DPYTHON_EXEC_PATH=${PYTHON_ENV_PATH} \
+      -DCMAKE_BUILD_TYPE={CPP_BUILD_MODE} -DCYLON_WITH_TEST={on_off(RUN_CPP_TESTS)} -DARROW_BUILD_TYPE=SYSTEM {CPPLINT_COMMAND} {INSTALL_COMMAND} \
       {CMAKE_FLAGS} \
       {CPP_SOURCE_DIR} || exit 1'
-    
+
     if BUILD_PYTHON:
         cmake_command = env_activate_command()+' && '+cmake_command
 
+    print(cmake_command)
     res = subprocess.call(cmake_command)
     check_status(res, "C++ cmake generate")
 
@@ -113,5 +151,47 @@ def build_cpp():
     res = subprocess.call(cmake_build_command)
     check_status(res, "C++ cmake build")
 
-build_cpp()
-# print(PYTHON_ENV_PATH)
+
+def build_docker():
+    if not BUILD_DOCKER:
+        return
+
+
+def python_test():
+    test_command = env_activate_command(
+    ) + ' && python -m pytest python/test/test_all.py || exit 1'
+    # ARROW_LIB=$(python3 -c 'import pyarrow as pa; import os; print(os.path.dirname(pa.__file__))') || exit 1
+    # export LD_LIBRARY_PATH="${ARROW_LIB}:${BUILD_PATH}/lib:${LD_LIBRARY_PATH}" || exit 1
+    # echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+    env = os.environ
+    env["LD_LIBRARY_PATH"] = f'{os.path.join(BUILD_DIR, "arrow", "install","lib")}:{os.path.join(BUILD_DIR, "lib")}:{os.environ.get("LD_LIBRARY_PATH","")}'
+    env["PATH"] = f'{os.path.join(BUILD_DIR, "arrow", "install","lib")}:{os.path.join(BUILD_DIR, "lib")}:{os.environ.get("PATH","")}'
+    res = subprocess.run(test_command, shell=True, env=env)
+    check_status(res, "Python test suite")
+
+
+def build_python():
+    if not BUILD_PYTHON:
+        return
+
+    print_line()
+    print("Building Python")
+
+    python_build_command = env_activate_command(
+    ) + f' && pip3 uninstall -y pycylon && python python/setup.py install || exit 1'
+
+    print(python_build_command)
+    env = os.environ
+    env["LD_LIBRARY_PATH"] = f'{os.path.join(BUILD_DIR, "arrow", "install","lib")}:{os.path.join(BUILD_DIR, "lib")}:{os.environ.get("LD_LIBRARY_PATH","")}'
+    env["CYLON_PREFIX"] = BUILD_DIR
+    env["ARROW_PREFIX"] = os.path.join(BUILD_DIR, "arrow", "install")
+
+    res = subprocess.run(python_build_command, shell=True, env=env)
+
+    check_status(res.returncode, "PyCylon build")
+
+
+# build_cpp()
+# build_docker()
+build_python()
+# python_test()
--- python/setup.py
+++ python/setup.py
@@ -28,6 +28,7 @@ import versioneer
 from Cython.Build import cythonize
 from setuptools import find_packages, setup
 from setuptools.extension import Extension
+import glob
 
 version = versioneer.get_version(),
 cmdclass = versioneer.get_cmdclass(),
@@ -45,7 +46,8 @@ except Exception:
     nthreads = 0
 
 compiler_directives = {"language_level": 3, "embedsignature": True}
-cython_files = ["pycylon/*/*.pyx"]
+cython_files = glob.glob(os.path.dirname(
+    os.path.abspath(__file__))+'/pycylon/*/*.pyx')
 
 if not CYLON_PREFIX:
     raise ValueError("CYLON_PREFIX not set")
@@ -58,35 +60,50 @@ arrow_library_directory = None
 if not ARROW_PREFIX:
     arrow_lib_include_dir = os.path.join(pyarrow_location, "include")
     arrow_library_directory = pyarrow_location
-    additional_compile_args = additional_compile_args + ['-D_GLIBCXX_USE_CXX11_ABI=0']
+    additional_compile_args = additional_compile_args + \
+        ['-D_GLIBCXX_USE_CXX11_ABI=0']
     if not os.path.exists(arrow_library_directory):
         arrow_library_directory = os.path.join(pyarrow_location, "lib64")
 else:
     arrow_lib_include_dir = os.path.join(ARROW_PREFIX, "include")
     arrow_library_directory = os.path.join(ARROW_PREFIX, "lib")
-
     if not os.path.exists(arrow_library_directory):
         arrow_library_directory = os.path.join(ARROW_PREFIX, "lib64")
 
 pyarrow_include_dir = os.path.join(pyarrow_location, 'include')
-extra_compile_args = os.popen("mpic++ --showme:compile").read().strip().split(' ')
-extra_link_args = os.popen("mpic++ --showme:link").read().strip().split(' ')
-extra_compile_args = extra_compile_args + extra_link_args + additional_compile_args
-extra_link_args.append("-Wl,-rpath,$ORIGIN/pyarrow")
+
+# extra_compile_args = os.popen(
+#     "mpic++ --showme:compile").read().strip().split(' ')
+# extra_link_args = os.popen("mpic++ --showme:link").read().strip().split(' ')
+# extra_compile_args = extra_compile_args + \
+#     extra_link_args + additional_compile_args
+# extra_link_args.append("-Wl,-rpath,$ORIGIN/pyarrow")
 
 glob_library_directory = os.path.join(CYLON_PREFIX, "glog", "install", "lib")
 
 glog_lib_include_dir = os.path.join(CYLON_PREFIX, "glog", "install", "include")
 cylon_library_directory = os.path.join(CYLON_PREFIX, "lib")
+cylon_library_directory_debug = os.path.join(CYLON_PREFIX, "lib", "Debug")
+cylon_library_directory_release = os.path.join(CYLON_PREFIX, "lib", "Release")
 
 library_directories = [cylon_library_directory,
+                       cylon_library_directory_debug,
+                       cylon_library_directory_release,
                        arrow_library_directory,
                        glob_library_directory,
                        get_python_lib(),
                        os.path.join(os.sys.prefix, "lib")]
 
-libraries = ["arrow", "cylon", "glog"]
-cylon_include_dir = "../cpp/src/"
+libraries = ["arrow", "cylon", "glogd"]
+cylon_include_dir = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", "cpp", "src")
+
+
+# print("###############", library_directories)
+# print("###############", libraries)
+print("###############", cylon_include_dir)
+
+# exit(0)
 
 _include_dirs = [cylon_include_dir,
                  arrow_lib_include_dir,
@@ -105,8 +122,8 @@ extensions = [
         sources=cython_files,
         include_dirs=_include_dirs,
         language='c++',
-        extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args,
+        # extra_compile_args=extra_compile_args,
+        # extra_link_args=extra_link_args,
         libraries=libraries,
         library_dirs=library_directories,
     )
