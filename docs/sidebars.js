1e5db7b686b7104cd10b6ef0290283dc43878c9b
Supun Kamburugamuve <supun06@gmail.com>
2021-08-30 22:27:17 -0400

    improve docs (#476)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -4,8 +4,8 @@ module.exports = {
     "Get Started": ['python', 'cpp'],
     "Deployment": ['mpi'],
     "Architecture": ['arch'],
-    "Publications": ['pub','blogs'],
     "API": [{ "type": "link", "label": "Python API docs", "href": "https://cylondata.org/pydocs/frame.html" }, { "type": "link", "label": "Javadocs", "href": "https://cylondata.org/javadocs/index.html" }],
-    "Releases": ["release/0.4.1", "release/0.4.0", "release/0.3.1", "release/0.3.0", "release/0.2.0", "release/0.1.0"]
+    "Releases": ["release/0.4.1", "release/0.4.0", "release/0.3.1", "release/0.3.0", "release/0.2.0", "release/0.1.0"],
+    "Publications": ['pub','blogs', 'contributors'],
   },
 };
