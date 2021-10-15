204ba52edb8f7bd5abd3bceb4d8f845c17024db7
niranda perera <niranda.perera@gmail.com>
2021-10-14 20:36:52 -0400

    adding arrow_rand utils

---
+++ cpp/src/cylon/util/arrow_rand.hpp
@@ -0,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CYLON_CPP_SRC_CYLON_UTIL_ARROW_RAND_HPP_
+#define CYLON_CPP_SRC_CYLON_UTIL_ARROW_RAND_HPP_
+
+#include <arrow/api.h>
+#include <random>
+
+namespace cylon {
+
+namespace {
+template<typename V>
+typename std::enable_if_t<std::is_integral<V>::value, void> GenerateTypedData(
+    V *data, size_t n, V &min_, V &max_, uint32_t seed = 0) {
+  std::default_random_engine rng(seed);
+  std::uniform_int_distribution<V> dist(min_, max_);
+
+  // A static cast is required due to the int16 -> int8 handling.
+  std::generate(data, data + n, [&] { return dist(rng); });
+}
+
+template<typename V>
+typename std::enable_if_t<std::is_floating_point<V>::value, void> GenerateTypedData(
+    V *data, size_t n, V &min_, V &max_, uint32_t seed = 0) {
+  std::default_random_engine rng(seed);
+  std::uniform_real_distribution<V> dist(min_, max_);
+
+  // A static cast is required due to the int16 -> int8 handling.
+  std::generate(data, data + n, [&] { return dist(rng); });
+}
+
+void GenerateBitmap(uint8_t *buffer,
+                    size_t n,
+                    double false_prob,
+                    int64_t *false_count,
+                    uint32_t seed = 0) {
+  int64_t count = 0;
+  std::default_random_engine rng(seed);
+  std::bernoulli_distribution dist(1.0 - false_prob);
+
+  for (size_t i = 0; i < n; i++) {
+    if (dist(rng)) {
+      arrow::BitUtil::SetBit(buffer, i);
+    } else {
+      count++;
+    }
+  }
+
+  if (false_count != nullptr) *false_count = count;
+}
+} // namespace
+
+struct RandomArrayGenerator {
+ public:
+  explicit RandomArrayGenerator(uint32_t seed = 0,
+                                arrow::MemoryPool *pool = arrow::default_memory_pool())
+      : seed(seed), pool(pool) {}
+
+  std::shared_ptr<arrow::Array> Boolean(int64_t size,
+                                        double true_probability,
+                                        double null_probability = 0.0) {
+    std::shared_ptr<arrow::Buffer> validity = nullptr, data;
+    int64_t null_count = 0;
+    if (null_probability > 0.0) {
+      validity = *arrow::AllocateEmptyBitmap(size, pool);
+      GenerateBitmap(validity->mutable_data(), size, null_probability, &null_count, seed++);
+    }
+
+    data = *arrow::AllocateEmptyBitmap(size, pool);
+    if (true_probability > 0.0) {
+      GenerateBitmap(validity->mutable_data(), size, 1 - true_probability, nullptr, seed++);
+    }
+
+    auto array_data = arrow::ArrayData::Make(arrow::boolean(),
+                                             size,
+                                             {std::move(validity), std::move(data)},
+                                             null_count);
+    return std::make_shared<arrow::BooleanArray>(array_data);
+  }
+
+  template<typename ArrowType, typename CType = typename ArrowType::c_type>
+  std::shared_ptr<arrow::Array> Numeric(int64_t size,
+                                        CType min,
+                                        CType max,
+                                        double null_probability = 0.0) {
+    std::shared_ptr<arrow::Buffer> validity = nullptr, data;
+    int64_t null_count = 0;
+    if (null_probability > 0.0) {
+      validity = *arrow::AllocateEmptyBitmap(size, pool);
+      GenerateBitmap(validity->mutable_data(), size, null_probability, &null_count, seed++);
+    }
+
+    data = *arrow::AllocateBuffer(size * sizeof(CType), pool);
+    auto *mut_data = reinterpret_cast<CType *>(data->mutable_data());
+    GenerateTypedData<CType>(mut_data, size, min, max, seed++);
+
+    auto array_data = arrow::ArrayData::Make(arrow::TypeTraits<ArrowType>::type_singleton(),
+                                             size,
+                                             {std::move(validity), std::move(data)},
+                                             null_count);
+    return std::make_shared<arrow::NumericArray<ArrowType>>(std::move(array_data));
+  }
+
+ private:
+  uint32_t seed;
+  arrow::MemoryPool *pool;
+};
+
+}
+
+#endif //CYLON_CPP_SRC_CYLON_UTIL_ARROW_RAND_HPP_
--- cpp/src/examples/example_utils.cpp
+++ cpp/src/examples/example_utils.cpp
@@ -13,77 +13,46 @@
  */
 #include "example_utils.hpp"
 
+#include <utility>
+#include "util/arrow_rand.hpp"
+
 namespace cylon {
 namespace examples {
 
-void create_table(uint64_t count, double dup,
-                        std::shared_ptr<cylon::CylonContext> &ctx,
-                        arrow::MemoryPool *pool,
-                        std::shared_ptr<arrow::Table> &left_table) {
-  arrow::Int64Builder left_id_builder(pool);
-  arrow::DoubleBuilder cost_builder(pool);
-  std::random_device rd;
-  std::mt19937_64 gen(0);
-  std::uniform_int_distribution<int64_t> distrib(0, (int64_t) (count * dup * ctx->GetWorldSize()));
+void create_table(int64_t count, double dup,
+                  std::shared_ptr<cylon::CylonContext> &ctx,
+                  arrow::MemoryPool *pool,
+                  std::shared_ptr<arrow::Table> &left_table,
+                  double null_prob) {
+  RandomArrayGenerator gen(/*seed=*/0, pool);
+  auto left = gen.Numeric<arrow::Int64Type>(count, 0L,
+                                            (int64_t) (count * ctx->GetWorldSize() * dup),
+                                            null_prob);
+  auto vals = gen.Numeric<arrow::DoubleType>(count, 0.0, 1.0, null_prob);
 
-  std::mt19937_64 gen1(rd());
-  std::uniform_real_distribution<double> val_distrib;
-
-  arrow::Status st = left_id_builder.Reserve(count);
-  st = cost_builder.Reserve(count);
-  for (uint64_t i = 0; i < count; i++) {
-    int64_t l = distrib(gen);
-    double v = val_distrib(gen1);
-    left_id_builder.UnsafeAppend(l);
-    cost_builder.UnsafeAppend(v);
-  }
-
-  std::shared_ptr<arrow::Array> left, vals;
-  st = left_id_builder.Finish(&left);
-  st = cost_builder.Finish(&vals);
-  std::vector<std::shared_ptr<arrow::Field>> left_schema_vector = {arrow::field("first", left->type()),
-                                                                   arrow::field("second", vals->type())};
-  auto left_schema = std::make_shared<arrow::Schema>(left_schema_vector);
-  left_table = arrow::Table::Make(left_schema, {std::move(left), vals});
+  auto left_schema = arrow::schema({arrow::field("first", left->type()),
+                                    arrow::field("second", vals->type())});
+  left_table = arrow::Table::Make(left_schema, {std::move(left), std::move(vals)});
 }
 
-void create_int64_table(uint64_t count, double dup,
+void create_int64_table(int64_t count, double dup,
                         std::shared_ptr<cylon::CylonContext> &ctx,
                         arrow::MemoryPool *pool,
                         std::shared_ptr<arrow::Table> &left_table,
                         std::shared_ptr<arrow::Table> &right_table) {
-  arrow::Int64Builder left_id_builder(pool), right_id_builder(pool);
-  arrow::DoubleBuilder cost_builder(pool);
-  std::random_device rd;
-  std::mt19937_64 gen(0);
-  std::uniform_int_distribution<int64_t> distrib(0, (int64_t) (count * dup * ctx->GetWorldSize()));
+  RandomArrayGenerator gen(/*seed=*/0, pool);
 
-  std::mt19937_64 gen1(rd());
-  std::uniform_real_distribution<double> val_distrib;
+  auto max = (int64_t) (count * dup * ctx->GetWorldSize());
+  auto left = gen.Numeric<arrow::Int64Type>(count, 0L, max);
+  auto right = gen.Numeric<arrow::Int64Type>(count, 0L, max);
+  auto vals = gen.Numeric<arrow::DoubleType>(count, 0.0, 1.0);
 
-  arrow::Status st = left_id_builder.Reserve(count);
-  st = right_id_builder.Reserve(count);
-  st = cost_builder.Reserve(count);
-  for (uint64_t i = 0; i < count; i++) {
-    int64_t l = distrib(gen);
-    int64_t r = distrib(gen);
-    double v = val_distrib(gen1);
-    left_id_builder.UnsafeAppend(l);
-    right_id_builder.UnsafeAppend(r);
-    cost_builder.UnsafeAppend(v);
-  }
-
-  std::shared_ptr<arrow::Array> left, right, vals;
-  st = left_id_builder.Finish(&left);
-  st = right_id_builder.Finish(&right);
-  st = cost_builder.Finish(&vals);
-  std::vector<std::shared_ptr<arrow::Field>> left_schema_vector = {arrow::field("first", left->type()),
-                                                                   arrow::field("second", vals->type())};
-  auto left_schema = std::make_shared<arrow::Schema>(left_schema_vector);
+  auto left_schema = arrow::schema({arrow::field("first", left->type()),
+                                    arrow::field("second", vals->type())});
   left_table = arrow::Table::Make(left_schema, {std::move(left), vals});
-  std::vector<std::shared_ptr<arrow::Field>> right_schema_vector = {arrow::field("first", right->type()),
-                                                                    arrow::field("second", vals->type())};
-  auto right_schema = std::make_shared<arrow::Schema>(right_schema_vector);
+
+  auto right_schema = arrow::schema({arrow::field("first", right->type()),
+                                     arrow::field("second", vals->type())});
   right_table = arrow::Table::Make(right_schema, {std::move(right), std::move(vals)});
 }
 
@@ -92,35 +61,41 @@ int create_two_in_memory_tables_from_arrow_tables(std::shared_ptr<cylon::CylonCo
                                                    std::shared_ptr<arrow::Table> right_table,
                                                    std::shared_ptr<cylon::Table> &first_table,
                                                    std::shared_ptr<cylon::Table> &second_table) {
-  auto status = cylon::Table::FromArrowTable(ctx, left_table, first_table);
+  auto status = cylon::Table::FromArrowTable(ctx, std::move(left_table), first_table);
   if (!status.is_ok()) {
     return 1;
   }
 
-  status = cylon::Table::FromArrowTable(ctx, right_table, second_table);
+  status = cylon::Table::FromArrowTable(ctx, std::move(right_table), second_table);
   if (!status.is_ok()) {
     return 1;
   }
   return 0;
 }
 
-int create_two_in_memory_tables(uint64_t count, double dup,
-                                 std::shared_ptr<cylon::CylonContext> &ctx,
-                                 std::shared_ptr<cylon::Table> &first_table,
-                                 std::shared_ptr<cylon::Table> &second_table) {
+int create_two_in_memory_tables(int64_t count, double dup,
+                                std::shared_ptr<cylon::CylonContext> &ctx,
+                                std::shared_ptr<cylon::Table> &first_table,
+                                std::shared_ptr<cylon::Table> &second_table) {
   arrow::MemoryPool *pool = arrow::default_memory_pool();
   std::shared_ptr<arrow::Table> left_table, right_table;
   cylon::examples::create_int64_table(count, dup, ctx, pool, left_table, right_table);
-  create_two_in_memory_tables_from_arrow_tables(ctx, left_table, right_table, first_table, second_table);
+  create_two_in_memory_tables_from_arrow_tables(ctx,
+                                                left_table,
+                                                right_table,
+                                                first_table,
+                                                second_table);
   return 0;
 }
 
-int create_in_memory_tables(uint64_t count, double dup,
-                                std::shared_ptr<cylon::CylonContext> &ctx,
-                                std::shared_ptr<cylon::Table> &first_table) {
+int create_in_memory_tables(int64_t count,
+                            double dup,
+                            std::shared_ptr<cylon::CylonContext> &ctx,
+                            std::shared_ptr<cylon::Table> &first_table,
+                            double null_prob) {
   arrow::MemoryPool *pool = arrow::default_memory_pool();
   std::shared_ptr<arrow::Table> left_table, right_table;
-  cylon::examples::create_table(count, dup, ctx, pool, left_table);
+  cylon::examples::create_table(count, dup, ctx, pool, left_table, null_prob);
   auto status = cylon::Table::FromArrowTable(ctx, left_table, first_table);
   if (!status.is_ok()) {
     return 1;
