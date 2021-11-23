81c7f5acddf7cbea2abdd528dc2e8fdc2c7ad74e
niranda perera <niranda.perera@gmail.com>
2021-11-23 12:14:41 -0500

    adding an additional test

--- python/pycylon/test/test_repartition.py
+++ python/pycylon/test/test_repartition.py
@@ -1,21 +1,45 @@
 from pycylon import CylonEnv
-from utils import create_df,assert_eq
+from utils import create_df, assert_eq
 from pycylon.net import MPIConfig
 import random
 
 """
 Run test:
->> pytest -q python/pycylon/test/test_repartition.py
+>> mpirun -n 4 python -m pytest --with-mpi -q python/pycylon/test/test_repartition.py
 """
 import pytest
 
 
 @pytest.mark.mpi
-def test_repartition():
-    env=CylonEnv(config=MPIConfig())
-    world_sz = env.world_size  
+def test_repartition_equal_parts():
+    env = CylonEnv(config=MPIConfig())
+    world_sz = env.world_size
     df1, _ = create_df([random.sample(range(10, 300), 50),
-                            random.sample(range(10, 300), 50),
-                            random.sample(range(10, 300), 50)])
-    df2 = df1.repartition([50 for _  in range(world_sz)], None, env=env) # distributed repartition 
-    assert_eq(df1, df2)  # still the local partitions would be equal 
+                        random.sample(range(10, 300), 50),
+                        random.sample(range(10, 300), 50)])
+    # distributed repartition
+    df2 = df1.repartition([50 for _ in range(world_sz)], None, env=env)
+
+    # still the local partitions would be equal
+    assert_eq(df1, df2)
+
+
+@pytest.mark.mpi
+def test_repartition():
+    env = CylonEnv(config=MPIConfig())
+    world_sz = env.world_size
+
+    local_rows = 50
+    global_rows = local_rows * world_sz
+
+    df1, _ = create_df([random.sample(range(10, 300), local_rows),
+                        random.sample(range(10, 300), local_rows),
+                        random.sample(range(10, 300), local_rows)])
+    # distributed repartition
+    temp = list(range(1, world_sz + 1))
+    s = sum(temp)
+
+    part_list = [global_rows * p / s for p in temp]
+    df2 = df1.repartition(part_list, None, env=env)
+
+    assert (len(df2) == part_list[env.rank])
