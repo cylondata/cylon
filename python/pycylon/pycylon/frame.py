7da4f1c4756d466689bf58ba2145aeaad589b9f0
kaiyingshan <shan_kaiying@163.com>
2021-12-22 06:05:45 -0500

    added io functions

--- python/pycylon/pycylon/frame.py
+++ python/pycylon/pycylon/frame.py
@@ -18,11 +18,15 @@ from __future__ import annotations
 from collections.abc import Iterable
 from copy import copy
 from typing import Hashable, List, Dict, Optional, Sequence, Union
+import os
 
 import numpy as np
 import pandas
 import pandas as pd
 import pyarrow as pa
+import pyarrow.json as pj
+import pyarrow.parquet as pq
+import pyarrow.csv as pac
 
 import pycylon as cn
 import pycylon.data as pcd
@@ -36,7 +40,7 @@ from pycylon.io import CSVWriteOptions
 
 from pycylon.indexing.cyindex import IndexingType
 from pycylon.indexing.cyindex import PyLocIndexer
-
+from pycylon.util import io_utils
 
 DEVICE_CPU = "cpu"
 
@@ -88,6 +92,40 @@ def read_csv(filepath: str, use_threads=True, names=None, sep=",", block_size: i
     table = pcd.csv.read_csv(ctx, filepath, read_config)
 
     return DataFrame(table)
+
+def _read_csv_or_json(read_fun, paths, env, **kwargs) -> DataFrame:
+    """
+    Read CSV or JSON files and construct a distributed DataFrame
+    """
+
+    if isinstance(paths, str):
+        paths = [paths]
+
+    if isinstance(paths, list) and all(isinstance(p, str) for p in paths):
+        worker_files = io_utils.get_worker_files(paths, env)
+    elif isinstance(paths, dict) and \
+            all(isinstance(key, int) and (isinstance(val, list) or isinstance(val, str)) for key, val in paths.items()):
+        worker_files = io_utils.get_files(paths[env.rank]) if env.rank in paths else []
+    else:
+        raise ValueError("paths must be: Union[str, List[str], Dict[int, Union[str, List[str]]]]")
+
+    df = None
+    # if there is only one file to read
+    if len(worker_files) == 1:
+        df = read_fun(worker_files[0], **kwargs)
+    elif len(worker_files) > 1:
+        df_list = []
+        for worker_file in worker_files:
+            df_list.append(read_fun(worker_file, **kwargs))
+        df = concat(df_list)
+
+    # schema = df .head(0).to_arrow().schema if cdf is not None else None
+    schema = df.to_arrow().schema
+    df_schema = io_utils.all_schemas_equal(schema, env=env)
+    if df is None:
+        df = DataFrame(df_schema.empty_table())
+
+    return df
 
 
 class CylonEnv(object):
@@ -2307,6 +2345,157 @@ class DataFrame(object):
         """
         return DataFrame(self._table.applymap(func))
 
+    @staticmethod
+    def read_csv(paths: Union[str, List[str], Dict[int, Union[str, List[str]]]],
+             env: CylonEnv, **kwargs) -> DataFrame:
+        return _read_csv_or_json(read_csv, paths, env, **kwargs)
+
+    @staticmethod
+    def read_json(paths: Union[str, List[str], Dict[int, Union[str, List[str]]]],
+             env: CylonEnv, **kwargs) -> DataFrame:
+        def json_read_fun(path, **kwargs):
+            return DataFrame(pj.read_json(path, **kwargs))
+        return _read_csv_or_json(json_read_fun, paths, env, **kwargs)
+
+    @staticmethod
+    def read_parquet(paths: Union[str, List[str], Dict[int, Union[str, List[str]]]],
+                 env: CylonEnv,
+                 **kwargs) -> DataFrame:
+        fmd = None
+        if isinstance(paths, str):
+            if os.path.isdir(paths):
+                mdfile = os.path.join(paths, "_metadata")
+                if os.path.isfile(mdfile):
+                    # all workers read the metadata file.
+                    # one worker may read and broadcast the metadata file.
+                    # since the metadata file is small, not sure this is needed
+                    fmd = pq.read_metadata(mdfile)
+            paths = [paths]
+
+        if fmd is None:
+            if isinstance(paths, list) and all(isinstance(p, str) for p in paths):
+                worker_files = io_utils.get_worker_files(paths, env, ext="*.parquet")
+                file_mappings_given = False
+            elif isinstance(paths, dict) and \
+                    all(isinstance(key, int) and (isinstance(val, list) or isinstance(val, str)) for key, val in paths.items()):
+                worker_files = io_utils.get_files(paths[env.rank], ext="*.parquet") if env.rank in paths else []
+                file_mappings_given = True
+            else:
+                raise ValueError("paths must be: Union[str, List[str], Dict[int, Union[str, List[str]]]]")
+
+            fmd = io_utils.all_gather_metadata(worker_files=worker_files, env=env)
+            if file_mappings_given:
+                df =  DataFrame(pq.read_table(worker_files, **kwargs)) if worker_files else \
+                    DataFrame(fmd.schema.to_arrow_schema().empty_table())
+                return df
+
+        # construct a DataFrame with relevant columns
+        pdf = io_utils.construct_df(fmd)
+
+        # distribute row_groups to workers
+        my_rg_df = io_utils.row_groups_this_worker(pdf, env)
+
+        my_files = my_rg_df["file"].unique().tolist()
+        my_row_groups = []
+        for f in my_files:
+            rg_per_file = my_rg_df[my_rg_df['file'] == f]["row_group_index"].tolist()
+            my_row_groups.append(rg_per_file)
+
+        if my_files:
+            df = pq.read_table(my_files, row_groups=my_row_groups, **kwargs)
+        else:
+            df = DataFrame(fmd.schema.to_arrow_schema().empty_table())
+        return df
+
+    @staticmethod
+    def write_csv(df: DataFrame,
+              dir_path: str,
+              env: CylonEnv,
+              name_function: callable = None,
+              **kwargs) -> str:
+        """
+        Write DataFrames to CSV files
+
+        Each worker writes a single CSV file.
+        All files are written to the given directory.
+        If the name_function is not provided:
+        each worker creates the output file with the pattern: "part_<rank>.csv"
+        If the name_function parameter is given:
+        this function is used to generate the output filename by each worker.
+        this function must take an int as the argument and return a string as the filename.
+        each worker calls this function with its worker rank.
+
+        Parameters
+        ----------
+        dir_path: Output directory for CSV files.
+        env: CylonEnv object for this DataFrame
+        name_function: a function to create the filename for that worker
+        kwargs: the parameters that will be passed on to cudf.DataFrame.to_csv function
+
+        Returns
+        -------
+        Filename written
+        """
+        outfile = io_utils.determine_file_name_to_write(dir_path=dir_path,
+                                                        name_function=name_function,
+                                                        file_ext="csv",
+                                                        env=env)
+        pac.write_csv(df.to_arrow(), outfile, **kwargs)
+        return outfile
+
+    @staticmethod
+    def write_json(df: DataFrame,
+               dir_path: str,
+               env: CylonEnv,
+               name_function: callable = None,
+               **kwargs) -> str:
+        """
+        Write DataFrames to JSON files
+
+        Each worker writes a single JSON file.
+        All files are written to the given directory.
+        If the name_function is not provided:
+        each worker creates the output file with the pattern: "part_<rank>.json"
+        If the name_function parameter is given:
+        this function is used to generate the output filename by each worker.
+        this function must take an int as the argument and return a string as the filename.
+        each worker calls this function with its worker rank.
+
+        Parameters
+        ----------
+        dir_path: Output directory for JSON files.
+        env: CylonEnv object for this DataFrame
+        name_function: a function to create the filename for that worker
+        kwargs: the parameters that will be passed on to cudf.DataFrame.to_json function
+
+        Returns
+        -------
+        Filename written
+        """
+        # convert to pandas dataframe b/c no to_json function available in pyarrow
+        outfile = io_utils.determine_file_name_to_write(dir_path=dir_path, name_function=name_function, file_ext="json", env=env)
+        pdf = df.to_pandas()
+        pdf.to_json(outfile, **kwargs)
+        return outfile
+
+    @staticmethod
+    def write_parquet(df: DataFrame,
+                  dir_path: str,
+                  env: CylonEnv,
+                  name_function: callable = None,
+                  write_metadata_file: bool = True,
+                  **kwargs) -> str:
+        outfile = io_utils.determine_file_name_to_write(dir_path=dir_path,
+                                                    name_function=name_function,
+                                                    file_ext="parquet",
+                                                    env=env)
+        pq.write_table(df, outfile, **kwargs)
+
+        if not write_metadata_file:
+            return outfile
+
+        io_utils.gather_metadata_save(outfile, env=env)
+        return outfile
 
 # -------------------- staticmethods ---------------------------
 
