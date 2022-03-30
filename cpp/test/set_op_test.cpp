a4aa691dd7d2eb9f2718d08be246cfa363b0595e
niranda <niranda.perera@gmail.com>
2022-03-30 00:52:20 -0400

    fixing set_op_test

--- cpp/test/set_op_test.cpp
+++ cpp/test/set_op_test.cpp
@@ -21,40 +21,40 @@
 namespace cylon {
 namespace test {
 
+void global_sum_equal(int64_t exp, int64_t val) {
+  auto rows = Scalar::Make(arrow::MakeScalar(val));
+  std::shared_ptr<Scalar> res;
+  CHECK_CYLON_STATUS(ctx->GetCommunicator()->AllReduce(rows, net::SUM, &res));
+  CHECK_ARROW_EQUAL(arrow::MakeScalar(exp), res->data());
+}
+
 TEST_CASE("Set operation testing", "[set_op]") {
   std::string path1 = "../data/input/csv1_" + std::to_string(RANK) + ".csv";
   std::string path2 = "../data/input/csv2_" + std::to_string(RANK) + ".csv";
   std::string out_path;
 
   SECTION("testing union") {
-    out_path = "../data/output/union_" + std::to_string(WORLD_SZ) + "_" + std::to_string(RANK) + ".csv";
+    out_path =
+        "../data/output/union_" + std::to_string(WORLD_SZ) + "_" + std::to_string(RANK) + ".csv";
     test::TestSetOperation(&DistributedUnion, ctx, path1, path2, out_path);
   }
 
   SECTION("testing union itself") {
     std::shared_ptr<Table> t, local, dist;
     CHECK_CYLON_STATUS(FromCSV(ctx, "../data/input/csv1_0.csv", t));
-
     CHECK_CYLON_STATUS(Union(t, t, local));
-
     CHECK_CYLON_STATUS(DistributedUnion(t, t, dist));
 
-    std::shared_ptr<compute::Result> res;
-    CHECK_CYLON_STATUS(compute::Count(dist, 0, res));
-    REQUIRE(std::static_pointer_cast<arrow::Int64Scalar>(res->GetResult().scalar())->value == local->Rows());
+    global_sum_equal(local->Rows(), dist->Rows());
   }
 
   SECTION("testing intersection itself") {
     std::shared_ptr<Table> t, local, dist;
     CHECK_CYLON_STATUS(FromCSV(ctx, "../data/input/csv1_0.csv", t));
-
     CHECK_CYLON_STATUS(Intersect(t, t, local));
-
     CHECK_CYLON_STATUS(DistributedIntersect(t, t, dist));
 
-    std::shared_ptr<compute::Result> res;
-    CHECK_CYLON_STATUS(compute::Count(dist, 0, res));
-    REQUIRE(std::static_pointer_cast<arrow::Int64Scalar>(res->GetResult().scalar())->value == local->Rows());
+    global_sum_equal(local->Rows(), dist->Rows());
   }
 
   SECTION("testing subtract") {
