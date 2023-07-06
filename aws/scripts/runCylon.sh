007dbc4adb50b39c593740bc22f3ad5cf3ebd1ae
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2023-07-06 10:44:32 -0400

    partial - first docker execution on aws

--- aws/scripts/runCylon.sh
+++ aws/scripts/runCylon.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-. /opt/conda/etc/profile.d/conda.sh
-conda activate cylon_dev
-export LD_LIBRARY_PATH=/cylon/install/lib
-echo $LD_LIBRARY_PATH
-python /cylon/aws/scripts/S3_run_script.py
+#!/bin/bash --login
+
+exec python /cylon/aws/scripts/S3_run_script.py
