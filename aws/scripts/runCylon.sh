6d012f22a737c2eaabe42532cc38226a8a1fc813
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2023-07-06 11:18:03 -0400

    partial - first docker execution on aws

--- aws/scripts/runCylon.sh
+++ aws/scripts/runCylon.sh
@@ -1,3 +1,15 @@
 #!/bin/bash --login
 
+# Enable strict mode.
+set -euo pipefail
+# ... Run whatever commands ...
+
+# Temporarily disable strict mode and activate conda:
+set +euo pipefail
+conda activate cylon_dev
+
+# Re-enable strict mode:
+set -euo pipefail
+
+export LD_LIBRARY_PATH=/cylon/install/lib
 exec python /cylon/aws/scripts/S3_run_script.py
