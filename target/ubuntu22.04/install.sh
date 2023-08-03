49db21d646b0129ee8ffef57aeda8ff315fa417d
Gregor von Laszewski <laszewski@gmail.com>
2023-08-03 11:37:07 -0400

    update install script

--- target/ubuntu22.04/install.sh
+++ target/ubuntu22.04/install.sh
@@ -1,14 +1,35 @@
+#
+#  time sh target/ubuntu22.04/install.sh 
+#
+PWD=`pwd`
+VENV=$PWD/build/CYLON
+echo "# ======================================================="
+echo "# prepare build dir
+echo "# ======================================================="
 rm -rf build
 mkdir build
-time python -m venv $PWD/build/CYLON
-source $PWD/build/CYLON/bin/activate
+echo "# ======================================================="
+echo "# Install python venv CYLON
+echo "# ======================================================="
+time python -m venv $VENV
+which python
+python --version
+source $VENV/bin/activate
 pip install pip -U
+pip install -r $PWD/target/ubuntu22.04/requirements.txt
+echo "# ======================================================="
+echo "# Compile cylon
+echo "# ======================================================="
 export CC=`which gcc`
 export CXX=`which g++`
 CC=gcc MPICC=mpicc pip install --no-binary mpi4py install mpi4py
-pip install -r $(PWD)/target/ubuntu22.04/requirements.txt
+
 BUILD_PATH=$PWD/build
 export LD_LIBRARY_PATH=$BUILD_PATH/arrow/install/lib64:$BUILD_PATH/glog/install/lib64:$BUILD_PATH/lib64:$BUILD_PATH/lib:$LD_LIBRARY_PATH
 
-time ./build.sh -pyenv $PWD/cy-rp-env -bpath $(pwd)/build --cpp --python_with_pyarrow --cython --test --cmake-flags "-DMPI_C_COMPILER=$(which mpicc) -DMPI_CXX_COMPILER=$(which mpicxx)"
+time ./build.sh -pyenv $VENV -bpath $PWD/build --cpp --python_with_pyarrow --cython --test --cmake-flags "-DMPI_C_COMPILER=$(which mpicc) -DMPI_CXX_COMPILER=$(which mpicxx)"
+
+echo "# ======================================================="
+echo "# Install Completed
+echo "# ======================================================="
 
