53d42d69e70c754d620feafc44aa92af1c45d3d1
Gregor von Laszewski <laszewski@gmail.com>
2023-08-03 11:48:40 -0400

    update install.sh

--- target/ubuntu22.04/install.sh
+++ target/ubuntu22.04/install.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
 #
 #  time sh target/ubuntu22.04/install.sh 
 #
 PWD=`pwd`
 VENV=$PWD/build/CYLON
+export BUILD_PATH=$PWD/build
 echo "# ======================================================="
 echo "# prepare build dir
 echo "# ======================================================="
@@ -11,7 +13,7 @@ mkdir build
 echo "# ======================================================="
 echo "# Install python venv CYLON
 echo "# ======================================================="
-time python -m venv $VENV
+python -m venv $VENV
 which python
 python --version
 source $VENV/bin/activate
@@ -24,7 +26,6 @@ export CC=`which gcc`
 export CXX=`which g++`
 CC=gcc MPICC=mpicc pip install --no-binary mpi4py install mpi4py
 
-BUILD_PATH=$PWD/build
 export LD_LIBRARY_PATH=$BUILD_PATH/arrow/install/lib64:$BUILD_PATH/glog/install/lib64:$BUILD_PATH/lib64:$BUILD_PATH/lib:$LD_LIBRARY_PATH
 
 time ./build.sh -pyenv $VENV -bpath $PWD/build --cpp --python_with_pyarrow --cython --test --cmake-flags "-DMPI_C_COMPILER=$(which mpicc) -DMPI_CXX_COMPILER=$(which mpicxx)"
