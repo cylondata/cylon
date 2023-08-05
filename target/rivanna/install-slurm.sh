c408b9e25458eaee815f0481bd6701c1fbe97f1c
Gregor von Laszewski <laszewski@gmail.com>
2023-08-05 09:51:19 -0400

    fis sh slurm script

--- target/rivanna/install-slurm.sh
+++ target/rivanna/install-slurm.sh
@@ -8,11 +8,8 @@
 
 SLURM_JOB_ID=$(sbatch --wait -A bii_dsc_community -p standard  -N 1 -c37 ./target/rivanna/install.sh | awk '{print $4}')
 
-# Wait for the job to complete
-srun --jobid=$SLURM_JOB_ID echo "Waiting for job $SLURM_JOB_ID to complete..."
-srun --jobid=$SLURM_JOB_ID sacct --format=JobID,End --noheader
-
 echo "Job $SLURM_JOB_ID is completed."
+
 
 echo "###############################################################"
 echo "# squeue"
@@ -21,41 +18,21 @@ echo "###############################################################"
 squeue --format="%.18i %.9P %.30j %.8u %.8T %.10M %.9l %.6D %R" -j $SLURM_JOB_ID
 
 echo "###############################################################"
-echo "# sstat"
-echo "###############################################################"
-
-sstat --jobs=$SLURM_JOB_ID
-
-echo "###############################################################"
 echo "# seff"
 echo "###############################################################"
 
-seff #SLURM_JOB_ID
-
-echo "###############################################################"
-echo "# sacct"
-echo "###############################################################"
-
-sacct -X  -j $SLURM_JOB_ID -o Reserved
-
-echo "###############################################################"
-echo "# sacct times"
-echo "###############################################################"
-
-sacct -X  -j $SLURM_JOB_ID -o CPUTime
-
-sacct -X  -j $SLURM_JOB_ID -o Submit
-sacct -X  -j $SLURM_JOB_ID -o Start
-sacct -X  -j $SLURM_JOB_ID -o End
-sacct -X  -j $SLURM_JOB_ID -o Elapsed
+seff $SLURM_JOB_ID
 
 echo "###############################################################"
 echo "# sacct analyze waittime"
 echo "###############################################################"
 
+reserved_time=$(sacct -X  -j $SLURM_JOB_ID -o Reserved --noheader)
 submit_time=$(sacct -X -j $SLURM_JOB_ID -o Submit --noheader)
 start_time=$(sacct -X -j $SLURM_JOB_ID -o Start --noheader)
 end_time=$(sacct -X -j $SLURM_JOB_ID -o End --noheader)
+cpu_time=$(sacct -X -j $SLURM_JOB_ID -o CPUTime --noheader)
+elapsed_time=$(sacct -X -j $SLURM_JOB_ID -o Elapse --noheader)
 
 # Convert timestamps to seconds since epoch
 submit_seconds=$(date -d "$submit_time" +"%s")
@@ -68,13 +45,17 @@ wait_time=$((start_seconds - submit_seconds))
 # Convert wait time to HH:MM:SS format
 wait_formatted=$(date -u -d @"$wait_time" +"%T")
 
-echo "JobID: $SLURM_JOB_ID"
-echo "Submit Time: $submit_time"
-echo "Start Time: $start_time"
-echo "End Time: $end_time"
-echo "Wait Time: $wait_formatted"
-echo "Wait Time in seconnds: $wait_time"
+echo "JobID:          $SLURM_JOB_ID"
+echo "CPU Time:       $cpu_time"
+echo "Elapsed Time:   $elapsed_time"
+echo "Researved Time: $reserved_time"
+echo "Submit Time:    $submit_time"
+echo "Start Time:     $start_time"
+echo "End Time:       $end_time"
+echo "Wait Time:      $wait_formatted"
+echo "Wait Time (s):  $wait_time"
 
 echo "###############################################################"
 
 echo "completed"
+
