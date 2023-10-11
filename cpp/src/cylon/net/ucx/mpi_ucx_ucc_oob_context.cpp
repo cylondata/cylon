450e2894a8abe73ece7d02bd1cdb2d31e2481cbe
mstaylor <bud.staylor@gmail.com>
2023-10-11 15:07:41 -0400

    removes cereal library

--- cpp/src/cylon/net/ucx/mpi_ucx_ucc_oob_context.cpp
+++ cpp/src/cylon/net/ucx/mpi_ucx_ucc_oob_context.cpp
@@ -48,7 +48,9 @@ std::shared_ptr<UCXOOBContext> UCCMPIOOBContext::makeUCXOOBContext() {
 
 OOBType UCCMPIOOBContext::Type() { return OOBType::OOB_MPI; }
 
-void *UCCMPIOOBContext::getCollInfo() { return MPI_COMM_WORLD; }
+void *UCCMPIOOBContext::getCollInfo() {
+    return static_cast<void*>(MPI_COMM_WORLD);
+}
 
 ucc_status_t UCCMPIOOBContext::oob_allgather(void *sbuf, void *rbuf,
                                              size_t msglen, void *coll_info,
@@ -58,12 +60,12 @@ ucc_status_t UCCMPIOOBContext::oob_allgather(void *sbuf, void *rbuf,
 
   MPI_Iallgather(sbuf, (int)msglen, MPI_BYTE, rbuf, (int)msglen, MPI_BYTE, comm,
                  &request);
-  *req = (void *)request;
+  *req = static_cast<void *>(request);
   return UCC_OK;
 }
 
 ucc_status_t UCCMPIOOBContext::oob_allgather_test(void *req) {
-  auto request = (MPI_Request)req;
+  auto request = static_cast<MPI_Request>(req);
   int completed;
 
   MPI_Test(&request, &completed, MPI_STATUS_IGNORE);
