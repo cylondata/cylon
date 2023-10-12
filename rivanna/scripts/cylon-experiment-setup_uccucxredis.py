80b7f61dfcffd51a866a16a9e7e2fe5f94e10a19
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2023-10-11 22:35:05 -0400

    partial - rivanna ucc/ucx/redis - fixes include library path for non-root installation of redis

--- rivanna/scripts/cylon-experiment-setup_uccucxredis.py
+++ rivanna/scripts/cylon-experiment-setup_uccucxredis.py
@@ -95,7 +95,6 @@ for nodes, threads, memory, rows, partition, exclusive in combination:
   #SBATCH --job-name=h-n={nodes:02d}-t={threads:02d}-e={e}
   #SBATCH --nodes={nodes}
   #SBATCH --ntasks={threads}
-  #SBATCH --mem={memory}
   #SBATCH --time=15:00
   #SBATCH --output=out-{nodes:02d}-{threads:02d}{jobid}.log
   #SBATCH --error=out-{nodes:02d}-{threads:02d}{jobid}.err
