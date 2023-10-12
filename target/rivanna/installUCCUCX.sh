21c600838ba085ed0f8b7a3d16d4221fb1859d05
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2023-10-12 11:02:27 -0400

    partial - rivanna ucc/ucx/redis - fixes include library path for non-root installation of redis

--- target/rivanna/installUCCUCX.sh
+++ target/rivanna/installUCCUCX.sh
@@ -25,10 +25,11 @@ CC=gcc MPICC=mpicc pip install --no-binary mpi4py install mpi4py
 UCC_INSTALL=/scratch/qad5gv/ucc/install
 UCX_INSTALL=/scratch/qad5gv/ucx/install
 REDIS_INSTALL=/scratch/qad5gv/redis_install
+pip install urllib3==1.26.6
 
 
 rm -rf build
 
 export LD_LIBRARY_PATH=$BUILD_PATH/install/lib:$UCC_INSTALL/lib:$REDIS_INSTALL/lib:$REDIS_INSTALL/lib64:$LD_LIBRARY_PATH
 
-time ./build.sh -j$(nproc) -pyenv $PWD/CYLON-ENV -bpath $PWD/build -ucxpath $UCX_INSTALL -uccpath $UCC_INSTALL -redispath $REDIS_INSTALL --cpp --python --cython --pytest --cmake-flags "-DMPI_C_COMPILER=$(which mpicc) -DMPI_CXX_COMPILER=$(which mpicxx) -DCYLON_UCX=1 -DUCX_INSTALL_PREFIX=$UCX_INSTALLl -DCYLON_UCC=1 -DUCC_INSTALL_PREFIX=$UCC_INSTALL -DCYLON_USE_REDIS=1 -DREDIS_INSTALL_PREFIX=$REDIS_INSTALL"
+time ./build.sh -j$(nproc) -pyenv $PWD/CYLON-ENV -bpath $PWD/build -ucxpath $UCX_INSTALL -uccpath $UCC_INSTALL -redispath $REDIS_INSTALL --cpp --python --cython --pytest --cmake-flags "-DMPI_C_COMPILER=$(which mpicc) -DMPI_CXX_COMPILER=$(which mpicxx) -DCYLON_UCX=1 -DUCX_INSTALL_PREFIX=$UCX_INSTALL -DCYLON_UCC=1 -DUCC_INSTALL_PREFIX=$UCC_INSTALL -DCYLON_USE_REDIS=1 -DREDIS_INSTALL_PREFIX=$REDIS_INSTALL"
