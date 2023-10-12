236a05296f3390eb39050688124748114c4a4e4a
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2023-10-12 11:06:07 -0400

    partial - rivanna ucc/ucx/redis - fixes include library path for non-root installation of redis

--- rivanna/scripts/cylon-experiment-setup_uccucxredis.py
+++ rivanna/scripts/cylon-experiment-setup_uccucxredis.py
@@ -38,7 +38,7 @@ args = vars(parser.parse_args())
 # (nodes, threads, cores, rows, partition, "exclusive")
 combination = [\
     # (1,4, 5000, "parallel", "exclusive"), # always pending
-    (2,4, "24G", args['rows'], "parallel", ""),
+    (1,2, "24G", args['rows'], "parallel", ""),
     #(2,37, 1000000, "parallel", ""),
     #(4,37, 35000000, "parallel", ""),
     #(6,37, 35000000, "parallel", ""),
