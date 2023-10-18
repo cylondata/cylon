f170218d1019843dc20beb034fa43e7511243fc9
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2023-10-18 11:58:20 -0400

    partial - rivanna ucc/ucx/redis - minor changes to support multiple jobs to multiple redis (scaling rivanna script)

--- rivanna/scripts/cylon-experiment-setup_uccucxredis.py
+++ rivanna/scripts/cylon-experiment-setup_uccucxredis.py
@@ -36,10 +36,10 @@ args = vars(parser.parse_args())
 # (nodes, threads, cpus, rows, partition, "exclusive")
 combination = [\
     # (1,4, 5000, "parallel", "exclusive"), # always pending
-    ("3.227.248.225", 4,16, 8, args['rows'], "parallel", ""),
-    ("54.224.3.74", 4,16, 8, args['rows'], "parallel", ""),
-    ("100.25.2.85", 4,16, 8, args['rows'], "parallel", ""),
-    ("3.84.28.20", 4,16, 8, args['rows'], "parallel", ""),
+    ("44.192.99.54", 4,4, 8, args['rows'], "parallel", ""),
+    ("3.82.235.0", 4,4, 8, args['rows'], "parallel", ""),
+    ("54.157.153.213", 4,4, 8, args['rows'], "parallel", ""),
+    ("44.192.25.12", 4,4, 8, args['rows'], "parallel", ""),
     #(2,37, 1000000, "parallel", ""),
     #(4,37, 35000000, "parallel", ""),
     #(6,37, 35000000, "parallel", ""),
