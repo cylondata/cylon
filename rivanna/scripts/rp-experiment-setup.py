22cecdf635d5f3dc9ce33d9af4f07667c2c8c50f
arupcsedu <arupcsedu@gmail.com>
2023-12-04 20:56:16 -0500

    Update rp-experiment-setup.py

--- rivanna/scripts/rp-experiment-setup.py
+++ rivanna/scripts/rp-experiment-setup.py
@@ -85,7 +85,7 @@ for nodes, threads, rows, partition, exclusive in combination:
   echo "..............................................................." 
   export RADICAL_LOG_LVL="DEBUG"
   export RADICAL_PROFILE="TRUE"
-  export RADICAL_PILOT_DBURL="mongodb://rct-tutorial:HXH7vExF7GvCeMWn@95.217.193.116:27017/rct-tutorial"
+  export RADICAL_PILOT_DBURL="mongodb://Your_mongo_db_url"
   echo "..............................................................."  
   lscpu
   echo "..............................................................."
